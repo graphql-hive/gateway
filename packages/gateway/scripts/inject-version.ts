a1bd4c61bfc437179d8e03c10adc32ee63fb583a
enisdenjo <denis@denelop.com>
2024-10-14 18:28:30 +0200

    build and cjs

---
+++ packages/gateway/scripts/inject-version.ts
@@ -0,0 +1,40 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { fileURLToPath, URL } from 'node:url';
+// @ts-expect-error tsx will allow this to work
+import pkg from '../package.json';
+
+const version = process.argv[2] || pkg.version;
+
+console.log(`Injecting version ${version} to build and bundle`);
+
+const source = '// @inject-version globalThis.__VERSION__ here';
+const inject = `globalThis.__VERSION__ = '${version}';`;
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+for (const file of [
+  // build
+  resolve(__dirname, '../dist/bin.js'),
+  resolve(__dirname, '../dist/bin.cjs'),
+  // bundle
+  resolve(__dirname, '../bundle/dist/bin.mjs'),
+  // binary bundle
+  resolve(__dirname, '../bundle/mesh-serve.cjs'),
+]) {
+  try {
+    const content = await readFile(file, 'utf-8');
+    if (content.includes(source)) {
+      await writeFile(file, content.replace(source, inject));
+    }
+    console.info(`✅ Version injected to "${file}"`);
+  } catch (e) {
+    if (Object(e).code === 'ENOENT') {
+      console.warn(
+        `⚠️ File does not exist and cannot have the version injected "${file}"`,
+      );
+    } else {
+      throw e;
+    }
+  }
+}
