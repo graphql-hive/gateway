bc358fe2a612222ce73871ff0b41ef965a92b3be
enisdenjo <denis@denelop.com>
2024-10-26 18:54:22 +0200

    odata

---
+++ e2e/odata-trippin/odata-trippin.e2e.ts
@@ -0,0 +1,65 @@
+import { createTenv } from '@internal/e2e';
+import { expect, it } from 'vitest';
+
+const { gateway } = createTenv(__dirname);
+
+it('executes a query', async () => {
+  const { execute } = await gateway({
+    supergraph: {
+      with: 'mesh',
+      services: [],
+    },
+  });
+  const result = await execute({
+    query: /* GraphQL */ `
+      query GetMe {
+        Me {
+          UserName
+          FirstName
+          LastName
+          Gender
+          FavoriteFeature
+          AddressInfo {
+            Address
+            City {
+              Name
+              Region
+              CountryRegion
+            }
+          }
+          Trips(queryOptions: { top: 1 }) {
+            Description
+          }
+        }
+      }
+    `,
+  });
+  expect(result).toMatchInlineSnapshot(`
+    {
+      "data": {
+        "Me": {
+          "AddressInfo": [
+            {
+              "Address": "P.O. Box 555",
+              "City": {
+                "CountryRegion": "United States",
+                "Name": "Lander",
+                "Region": "WY",
+              },
+            },
+          ],
+          "FavoriteFeature": "Feature1",
+          "FirstName": "April",
+          "Gender": "Female",
+          "LastName": "Cline",
+          "Trips": [
+            {
+              "Description": "Trip in US",
+            },
+          ],
+          "UserName": "aprilcline",
+        },
+      },
+    }
+  `);
+});
