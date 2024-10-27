e3aa602e215ce1e6addca68301131162b90b7512
enisdenjo <denis@denelop.com>
2024-10-27 14:34:20 +0100

    less talking

--- internal/testing/src/getLocalhost.ts
+++ internal/testing/src/getLocalhost.ts
@@ -7,29 +7,25 @@ export async function getLocalhost(port: number) {
   const timeoutSignal = AbortSignal.timeout(5000);
   while (!timeoutSignal.aborted) {
     for (const hostname of hostnames) {
+      const url = `http://${hostname}:${port}`;
       if (isDebug()) {
-        console.log(`getLocalhost(port): Trying hostname: ${hostname}`);
+        console.log(`getLocalhost(port): Trying ${url}`);
       }
       try {
-        const res = await fetch(`http://${hostname}:${port}`, {
-          signal: timeoutSignal,
-        });
-        await res.text();
+        await fetch(url, { signal: timeoutSignal });
       } catch (e) {
         if (isDebug()) {
-          console.log(
-            `getLocalhost(port): Failed to connect to hostname: ${hostname}`,
-          );
+          console.log(`getLocalhost(port): Failed to connect on ${url}`);
         }
         continue;
       }
       if (isDebug()) {
-        console.log(`getLocalhost(port): Found hostname: ${hostname}`);
+        console.log(`getLocalhost(port): ${url} is available`);
       }
       return `http://${hostname}`;
     }
   }
   throw new Error(
-    `No available hostname found as a local hostname for the given port: ${port}. Tried hostnames: ${hostnames.join(', ')}`,
+    `No available hostname found locally for port ${port}. Tried ${hostnames.join(', ')} hostnames.`,
   );
 }
