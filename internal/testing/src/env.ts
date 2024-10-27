5b305221b3276b51e5f43ee4cef3d0507b7b88b2
enisdenjo <denis@denelop.com>
2024-10-27 19:43:40 +0100

    some e2es wont work outside linux

--- internal/testing/src/env.ts
+++ internal/testing/src/env.ts
@@ -1,9 +1,32 @@
+import os from 'node:os';
+
 /** Checks whether the `DEBUG` environment variable is truthy. */
 export function isDebug() {
   return boolEnv('DEBUG');
 }
 
+/** Checks whether the `CI` environment variable is truthy. */
+export function isCI() {
+  return boolEnv('CI');
+}
+
 /** Checks if the environment variable with {@link name} is truthy. */
 export function boolEnv(name: string) {
   return ['1', 't', 'true', 'y', 'yes'].includes(String(process.env[name]));
+}
+
+/** Returns `true` if the OS platform is one of the provided {@link platforms}. */
+export function isPlatform(...platforms: NodeJS.Platform[]): boolean {
+  return platforms.includes(os.platform());
+}
+
+/** Returns `true` if the OS platform is not any of the provided {@link platforms}. */
+export function isNotPlatform(...platforms: NodeJS.Platform[]): boolean {
+  const p = os.platform();
+  for (const andNot of platforms) {
+    if (p === andNot) {
+      return false;
+    }
+  }
+  return true;
 }
--- vitest.projects.ts
+++ vitest.projects.ts
@@ -1,5 +1,6 @@
 import { defineWorkspace } from 'vitest/config';
 import { timeout as testTimeout } from './internal/e2e/src/timeout';
+import { isCI, isNotPlatform } from './internal/testing/src/env';
 
 export default defineWorkspace([
   {
@@ -13,7 +14,19 @@ export default defineWorkspace([
     extends: './vitest.config.ts',
     test: {
       name: 'e2e',
-      include: ['**/*.e2e.ts'],
+      include: [
+        '**/*.e2e.ts',
+        ...(isCI() && isNotPlatform('linux')
+          ? [
+              // TODO: containers are not starting on non-linux environments
+              '!**/e2e/auto-type-merging',
+              '!**/e2e/neo4j-example',
+              '!**/e2e/soap-demo',
+              '!**/e2e/mysql-employees',
+              '!**/e2e/opentelemetry',
+            ]
+          : []),
+      ],
       hookTimeout: testTimeout,
       testTimeout,
     },
