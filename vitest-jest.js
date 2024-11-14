54b3273d0d9033d32f549a6b937921426c35b08b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-11-14 16:51:39 +0300

    Format

--- vitest-jest.js
+++ vitest-jest.js
@@ -18,13 +18,13 @@ module.exports = new Proxy(require('@jest/globals'), {
           return jestGlobals.describe.skip(`${name} >`, ...args);
         }
         return jestGlobals.describe.skip(name, ...args);
-      }
+      };
       describeFn.only = function describeOnly(name, ...args) {
         if (typeof name === 'string') {
           return jestGlobals.describe.only(`${name} >`, ...args);
         }
         return jestGlobals.describe.only(name, ...args);
-      }
+      };
       return describeFn;
     }
     return Reflect.get(jestGlobals, prop, receiver);
