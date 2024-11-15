424fc4a858ad266c012a26af5aee90d0e5a670a9
enisdenjo <badurinadenis@gmail.com>
2024-11-15 15:46:40 +0100

    inline modules and avoid build during tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
+// By default, Vite bypasses node_packages to native Node; meaning, imports to
+// packages that match the tsconfig paths wont work because Node will require the
+// packages as per the Node resolution spec.
+//
+// Vite will process inlined modules.
+const inline = [/@graphql-mesh\/runtime/, /@graphql-mesh\/fusion-composition/];
+
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: { server: { deps: { inline } } },
   resolve: {
     alias: {
       graphql: 'graphql/index.js', // TODO: why duplicate graphql errors when there's no multiple graphqls installed? mistery
