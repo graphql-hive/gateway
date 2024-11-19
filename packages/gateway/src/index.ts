1a6229b23680dafdbacb28b7b3a23f62c01061ee
enisdenjo <denis@denelop.com>
2024-11-19 19:48:35 +0100

    reexp

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -1,6 +1,6 @@
 export * from './cli';
 export * from '@graphql-hive/gateway-runtime';
-export { PubSub } from '@graphql-mesh/utils';
+export { PubSub, LogLevel, DefaultLogger } from '@graphql-mesh/utils';
 export * from '@graphql-mesh/plugin-jwt-auth';
 export * from '@graphql-mesh/plugin-opentelemetry';
 export * from '@graphql-mesh/plugin-prometheus';
