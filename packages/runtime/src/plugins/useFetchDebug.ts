803e3f976fd0e9ee2637ca1a3d44b28bed34c856
enisdenjo <denis@denelop.com>
2024-11-20 20:16:35 +0100

    more lazy

--- packages/runtime/src/plugins/useFetchDebug.ts
+++ packages/runtime/src/plugins/useFetchDebug.ts
@@ -9,14 +9,14 @@ export function useFetchDebug<TContext extends Record<string, any>>(opts: {
     onFetch({ url, options, logger = opts.logger }) {
       logger = logger.child('fetch');
       const fetchId = crypto.randomUUID();
-      logger.debug('request', {
+      logger.debug('request', () => ({
         fetchId,
         url,
         ...(options || {}),
         body: options?.body && JSON.stringify(options.body, null, '  '),
         headers:
           options?.headers && JSON.stringify(options.headers, null, '  '),
-      });
+      }));
       return function onFetchDone({ response }) {
         logger.debug('response', () => ({
           fetchId,
