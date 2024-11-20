0e8875efa3a16c70de001b878b78c1a438121859
enisdenjo <denis@denelop.com>
2024-11-20 21:07:57 +0100

    string

--- packages/runtime/src/plugins/useFetchDebug.ts
+++ packages/runtime/src/plugins/useFetchDebug.ts
@@ -9,24 +9,31 @@ export function useFetchDebug<TContext extends Record<string, any>>(opts: {
     onFetch({ url, options, logger = opts.logger }) {
       logger = logger.child('fetch');
       const fetchId = generateUUID();
-      logger.debug('request', () => ({
-        fetchId,
-        url,
-        ...(options || {}),
-        body: options?.body && JSON.stringify(options.body, null, '  '),
-        headers:
-          options?.headers && JSON.stringify(options.headers, null, '  '),
-      }));
+      logger.debug('request', () =>
+        JSON.stringify(
+          {
+            fetchId,
+            url,
+            ...(options || {}),
+            body: options?.body,
+            headers: options?.headers,
+          },
+          null,
+          '  ',
+        ),
+      );
       return function onFetchDone({ response }) {
-        logger.debug('response', () => ({
-          fetchId,
-          status: response.status,
-          headers: JSON.stringify(
-            Object.fromEntries(response.headers.entries()),
+        logger.debug('response', () =>
+          JSON.stringify(
+            {
+              fetchId,
+              status: response.status,
+              headers: Object.fromEntries(response.headers.entries()),
+            },
             null,
             '  ',
           ),
-        }));
+        );
       };
     },
   };
