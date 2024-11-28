5a326f22d7b2facec59f786712fbc80d3951aedb
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-11-28 14:32:13 +0300

    Format

--- scripts/pin-workspace-deps.js
+++ scripts/pin-workspace-deps.js
@@ -1,20 +1,24 @@
-import { globSync } from "fs";
+import { globSync } from 'fs';
 
-const foundPackageJsonFiles = globSync("packages/*/package.json");
+const foundPackageJsonFiles = globSync('packages/*/package.json');
 
 foundPackageJsonFiles.forEach(async (packageJsonFile) => {
-    const packageJson = require(packageJsonFile);
-    const dependencyProps = ["dependencies", "devDependencies", "peerDependencies"];
-    for (const depProp of dependencyProps) {
-        const dependencies = packageJson[depProp];
-        if (dependencies) {
-            for (const depName in dependencies) {
-                const depVersion = dependencies[depName];
-                if (depVersion === 'workspace:^') {
-                    dependencies[depName] = `workspace:*`;
-                }
-            }
+  const packageJson = require(packageJsonFile);
+  const dependencyProps = [
+    'dependencies',
+    'devDependencies',
+    'peerDependencies',
+  ];
+  for (const depProp of dependencyProps) {
+    const dependencies = packageJson[depProp];
+    if (dependencies) {
+      for (const depName in dependencies) {
+        const depVersion = dependencies[depName];
+        if (depVersion === 'workspace:^') {
+          dependencies[depName] = `workspace:*`;
         }
+      }
     }
-    fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2));
-})
+  }
+  fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2));
+});
