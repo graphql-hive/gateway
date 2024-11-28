bb7a3b777ec326757a64193d86c475aab3dd8f32
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-11-28 14:32:00 +0300

    Fix alpha releases

---
+++ scripts/pin-workspace-deps.js
@@ -0,0 +1,20 @@
+import { globSync } from "fs";
+
+const foundPackageJsonFiles = globSync("packages/*/package.json");
+
+foundPackageJsonFiles.forEach(async (packageJsonFile) => {
+    const packageJson = require(packageJsonFile);
+    const dependencyProps = ["dependencies", "devDependencies", "peerDependencies"];
+    for (const depProp of dependencyProps) {
+        const dependencies = packageJson[depProp];
+        if (dependencies) {
+            for (const depName in dependencies) {
+                const depVersion = dependencies[depName];
+                if (depVersion === 'workspace:^') {
+                    dependencies[depName] = `workspace:*`;
+                }
+            }
+        }
+    }
+    fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2));
+})
