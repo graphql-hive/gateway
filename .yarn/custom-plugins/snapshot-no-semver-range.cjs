24875d715bb5f06f8d30290b9664a70f153e58cf
Denis Badurina <denis@denelop.com>
2024-11-28 15:42:40 +0100

    Uses exact dependency versions for snapshot releases (#213)

---
+++ .yarn/custom-plugins/snapshot-no-semver-range.cjs
@@ -0,0 +1,67 @@
+module.exports = {
+  name: 'snapshot-no-semver-range',
+  /**
+   * @param {typeof require} require
+   * @returns {import('@yarnpkg/core').Plugin<import('@yarnpkg/plugin-pack').Hooks>} */
+  factory(require) {
+    /** @type {import('@yarnpkg/core')} */
+    const { structUtils } = require('@yarnpkg/core');
+    return {
+      hooks: {
+        /** @param {Record<string, any>} packageJson  */
+        beforeWorkspacePacking(workspace, packageJson) {
+          console.group('snapshot-no-semver-range');
+          console.log('Finding snapshot workspace dependencies...');
+          for (const category of [
+            'dependencies',
+            'devDependencies',
+            'peerDependencies',
+          ]) {
+            for (const desc of workspace.manifest
+              .getForScope(category)
+              .values()) {
+              const range = structUtils.parseRange(desc.range);
+              if (range.protocol !== 'workspace:') {
+                // we dont care about deps outside our workspaces
+                continue;
+              }
+
+              // find the matching workspace for the dependency
+              const matchingWorkspace =
+                workspace.project.tryWorkspaceByDescriptor(desc);
+              if (!matchingWorkspace) {
+                throw new Error(
+                  `Dependency workspace "${desc.name}" not found`,
+                );
+              }
+
+              if (!['^', '~'].includes(range.selector)) {
+                // keep as is if the version is not ranged
+                continue;
+              }
+
+              const version = matchingWorkspace.manifest.version;
+              if (!version) {
+                throw new Error(
+                  `Dependency workspace "${desc.name}" does not have a version set`,
+                );
+              }
+              if (!version.includes('-')) {
+                // if the version does not include a dash, it's probably not a snapshot - we want to range it (keep as is)
+                continue;
+              }
+
+              const name = structUtils.stringifyIdent(desc);
+              console.log(
+                `Setting "${name}" to exact snapshot version "${version}" (from ranged "workspace:${range.selector}")`,
+              );
+              packageJson[category][name] = version;
+            }
+          }
+          console.log('Done!');
+          console.groupEnd();
+        },
+      },
+    };
+  },
+};
