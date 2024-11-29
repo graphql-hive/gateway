1065c9d3f154fe319525ac9b87b55857cf2ce13b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-11-29 13:15:21 +0300

    Fix Docker

--- e2e/openapi-subgraph/openapi-subgraph.e2e.ts
+++ e2e/openapi-subgraph/openapi-subgraph.e2e.ts
@@ -2,51 +2,42 @@ import { createTenv, Service } from '@internal/e2e';
 import { beforeAll, describe, expect, it } from 'vitest';
 
 describe('openapi-subgraph', () => {
-  let TestService: Service;
-  const { service, composeWithMesh, gateway } = createTenv(__dirname);
-  beforeAll(async () => {
-    TestService = await service('Test');
-  });
-  it('generates a subgraph SDL from an OpenAPI spec', async () => {
-    const { result } = await composeWithMesh({
-      services: [TestService],
-      maskServicePorts: true,
-      args: ['--subgraph', 'Test'],
+    let TestService: Service;
+    const { service, composeWithMesh, gateway } = createTenv(__dirname);
+    beforeAll(async () => {
+        TestService = await service('Test');
     });
-    expect(result).toMatchSnapshot();
-  });
-  it('exposes the SDL correctly', async () => {
-    const { result, output } = await composeWithMesh({
-      services: [TestService],
-      args: ['--subgraph', 'Test'],
-      output: 'graphql',
-    });
-    const { execute } = await gateway({
-      args: ['subgraph', output],
-    });
-    const sdlQuery = /* GraphQL */ `
+    it('exposes the SDL correctly', async () => {
+        const { result, output } = await composeWithMesh({
+            services: [TestService],
+            args: ['--subgraph', 'Test'],
+            output: 'graphql',
+        });
+        const { execute } = await gateway({
+            subgraph: output,
+        });
+        const sdlQuery = /* GraphQL */ `
       query {
         _service {
           sdl
         }
       }
     `;
-    const queryResult = await execute({
-      query: sdlQuery,
+        const queryResult = await execute({
+            query: sdlQuery,
+        });
+        expect(queryResult?.errors).toBeFalsy();
+        expect(queryResult?.data?._service?.sdl).toBe(result);
     });
-    expect(queryResult?.errors).toBeFalsy();
-    expect(queryResult?.data?._service?.sdl).toBe(result);
-  });
-  it('resolves entitites correctly', async () => {
-    const { output } = await composeWithMesh({
-      services: [TestService],
-      args: ['--subgraph', 'Test'],
-      output: 'graphql',
-    });
-    const { execute } = await gateway({
-      args: ['subgraph', output],
-    });
-    const entitiesQuery = /* GraphQL */ `
+    it('resolves entitites correctly', async () => {
+        const { execute } = await gateway({
+            subgraph: {
+                with: 'mesh',
+                services: [TestService],
+                subgraphName: 'Test',
+            }
+        });
+        const entitiesQuery = /* GraphQL */ `
       query {
         _entities(representations: [{ __typename: "User", id: 1 }]) {
           __typename
@@ -57,30 +48,29 @@ describe('openapi-subgraph', () => {
         }
       }
     `;
-    const queryResult = await execute({
-      query: entitiesQuery,
+        const queryResult = await execute({
+            query: entitiesQuery,
+        });
+        expect(queryResult?.errors).toBeFalsy();
+        expect(queryResult?.data).toEqual({
+            _entities: [
+                {
+                    __typename: 'User',
+                    id: '1',
+                    name: 'Alice',
+                },
+            ],
+        });
     });
-    expect(queryResult?.errors).toBeFalsy();
-    expect(queryResult?.data).toEqual({
-      _entities: [
-        {
-          __typename: 'User',
-          id: '1',
-          name: 'Alice',
-        },
-      ],
-    });
-  });
-  it('encapsulates the queries correctly', async () => {
-    const { output } = await composeWithMesh({
-      services: [TestService],
-      args: ['--subgraph', 'TestEncapsulated'],
-      output: 'graphql',
-    });
-    const { execute } = await gateway({
-      args: ['subgraph', output],
-    });
-    const encapsulatedQuery = /* GraphQL */ `
+    it('encapsulates the queries correctly', async () => {
+        const { execute } = await gateway({
+            subgraph: {
+                with: 'mesh',
+                services: [TestService],
+                subgraphName: 'TestEncapsulated',
+            }
+        });
+        const encapsulatedQuery = /* GraphQL */ `
       {
         test {
           users {
@@ -90,27 +80,27 @@ describe('openapi-subgraph', () => {
         }
       }
     `;
-    const queryResult = await execute({
-      query: encapsulatedQuery,
+        const queryResult = await execute({
+            query: encapsulatedQuery,
+        });
+        expect(queryResult?.errors).toBeFalsy();
+        expect(queryResult?.data).toEqual({
+            test: {
+                users: [
+                    {
+                        id: '1',
+                        name: 'Alice',
+                    },
+                    {
+                        id: '2',
+                        name: 'Bob',
+                    },
+                    {
+                        id: '3',
+                        name: 'Charlie',
+                    },
+                ],
+            },
+        });
     });
-    expect(queryResult?.errors).toBeFalsy();
-    expect(queryResult?.data).toEqual({
-      test: {
-        users: [
-          {
-            id: '1',
-            name: 'Alice',
-          },
-          {
-            id: '2',
-            name: 'Bob',
-          },
-          {
-            id: '3',
-            name: 'Charlie',
-          },
-        ],
-      },
-    });
-  });
 });
