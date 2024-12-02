161430f102b1d48053e5a987b7cba37f06109865
enisdenjo <badurinadenis@gmail.com>
2024-12-02 18:49:41 +0100

    cloudflare test federation only

--- e2e/cloudflare-workers/cloudflare-workers.e2e.ts
+++ e2e/cloudflare-workers/cloudflare-workers.e2e.ts
@@ -1,202 +1,127 @@
-import {
-  createTenv,
-  createTjaeger,
-  getAvailablePort,
-  waitForPort,
-} from '@internal/e2e';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { createTenv, getAvailablePort, waitForPort } from '@internal/e2e';
 import { isDebug } from '@internal/testing';
 import { fetch } from '@whatwg-node/fetch';
 import { ExecutionResult } from 'graphql';
-import { describe, expect, it } from 'vitest';
+import { expect, it } from 'vitest';
 
-const { spawn, gatewayRunner } = createTenv(__dirname);
-const jaeger = createTjaeger(__dirname);
+const { spawn, gatewayRunner, service, composeWithMesh } =
+  createTenv(__dirname);
 
-describe.skipIf(gatewayRunner !== 'node')('Cloudflare Workers', () => {
-  const TEST_QUERY = /* GraphQL */ `
-    query TestQuery {
-      language(code: "en") {
-        name
-      }
-    }
-  `;
-
-  async function wrangler(env: {
-    OTLP_EXPORTER_URL: string;
-    OTLP_SERVICE_NAME: string;
-  }) {
-    const port = await getAvailablePort();
-    const [proc] = await spawn('yarn wrangler', {
-      pipeLogs: true, // TODO: remove
-      args: [
-        'dev',
-        '--port',
-        port.toString(),
-        '--var',
-        'OTLP_EXPORTER_URL:' + env.OTLP_EXPORTER_URL,
-        '--var',
-        'OTLP_SERVICE_NAME:' + env.OTLP_SERVICE_NAME,
-        ...(isDebug() ? ['--var', 'DEBUG:1'] : []),
-      ],
-    });
-    const signal = AbortSignal.timeout(3_000);
-    await waitForPort(port, signal);
-    return {
-      proc,
-      url: `http://0.0.0.0:${port}`,
-      async execute({
-        query,
-        headers,
-      }: {
-        query: string;
-        headers?: HeadersInit;
-      }): Promise<ExecutionResult> {
-        const r = await fetch(`http://0.0.0.0:${port}/graphql`, {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json',
-            ...headers,
-          },
-          body: JSON.stringify({ query }),
-        });
-        return r.json();
-      },
-    };
-  }
-
-  it('should report telemetry metrics correctly to jaeger', async () => {
-    const { env, getTraces } = await jaeger.start();
-    const { execute, proc } = await wrangler(env);
-    await expect(execute({ query: TEST_QUERY })).resolves
-      .toMatchInlineSnapshot(`
-      {
-        "data": {
-          "language": {
-            "name": "English",
-          },
-        },
-      }
-    `);
-    await proc[Symbol.asyncDispose](); // disposing the gateway will/should flush the traces
-
-    const traces = await getTraces();
-    expect(traces.data.length).toBe(2);
-    const relevantTraces = traces.data.filter((trace) =>
-      trace.spans.some((span) => span.operationName === 'POST /graphql'),
-    );
-    expect(relevantTraces.length).toBe(1);
-    const relevantTrace = relevantTraces[0];
-    expect(relevantTrace).toBeDefined();
-    expect(relevantTrace?.spans.length).toBe(5);
-
-    expect(relevantTrace?.spans).toContainEqual(
-      expect.objectContaining({ operationName: 'POST /graphql' }),
-    );
-    expect(relevantTrace?.spans).toContainEqual(
-      expect.objectContaining({ operationName: 'graphql.parse' }),
-    );
-    expect(relevantTrace?.spans).toContainEqual(
-      expect.objectContaining({ operationName: 'graphql.validate' }),
-    );
-    expect(relevantTrace?.spans).toContainEqual(
-      expect.objectContaining({ operationName: 'graphql.execute' }),
-    );
-    expect(
-      relevantTrace?.spans.filter((r) =>
-        r.operationName.includes('subgraph.execute'),
-      ).length,
-    ).toBe(1);
+async function wrangler() {
+  const port = await getAvailablePort();
+  const [proc] = await spawn('yarn wrangler', {
+    pipeLogs: true, // TODO: remove
+    args: [
+      'dev',
+      '--port',
+      port.toString(),
+      ...(isDebug() ? ['--var', 'DEBUG:1'] : []),
+    ],
   });
-
-  it('should report http failures', async () => {
-    const { env, getTraces } = await jaeger.start();
-    const { url, proc } = await wrangler(env);
-    await fetch(`${url}/non-existing`);
-    await proc[Symbol.asyncDispose](); // disposing the gateway will/should flush the traces
-
-    const traces = await getTraces();
-    expect(traces.data.length).toBe(2);
-    const relevantTrace = traces.data.find((trace) =>
-      trace.spans.some((span) => span.operationName === 'GET /non-existing'),
-    );
-    expect(relevantTrace).toBeDefined();
-    expect(relevantTrace?.spans.length).toBe(1);
-
-    expect(relevantTrace?.spans).toContainEqual(
-      expect.objectContaining({
-        operationName: 'GET /non-existing',
-        tags: expect.arrayContaining([
-          expect.objectContaining({
-            key: 'otel.status_code',
-            value: 'ERROR',
-          }),
-          expect.objectContaining({
-            key: 'error',
-            value: true,
-          }),
-          expect.objectContaining({
-            key: 'http.status_code',
-            value: 404,
-          }),
-        ]),
-      }),
-    );
-  });
-
-  it('context propagation should work correctly', async () => {
-    const { env, getTraces } = await jaeger.start();
-    const { url, execute, proc } = await wrangler(env);
-    const traceId = '0af7651916cd43dd8448eb211c80319c';
-    await expect(
-      execute({
-        query: TEST_QUERY,
+  const signal = AbortSignal.timeout(3_000);
+  await waitForPort(port, signal);
+  return {
+    proc,
+    url: `http://0.0.0.0:${port}`,
+    async execute({
+      query,
+      headers,
+    }: {
+      query: string;
+      headers?: HeadersInit;
+    }): Promise<ExecutionResult> {
+      const r = await fetch(`http://0.0.0.0:${port}/graphql`, {
+        method: 'POST',
         headers: {
-          traceparent: `00-${traceId}-b7ad6b7169203331-01`,
+          'content-type': 'application/json',
+          ...headers,
         },
-      }),
-    ).resolves.toMatchInlineSnapshot(`
-          {
-            "data": {
-              "language": {
-                "name": "English",
-              },
-            },
-          }
-        `);
-    const upstreamHttpCalls = await fetch(`${url}/upstream-fetch`).then(
-      (r) =>
-        r.json() as unknown as Array<{
-          url: string;
-          headers?: Record<string, string>;
-        }>,
-    );
-    await proc[Symbol.asyncDispose](); // disposing the gateway will/should flush the traces
+        body: JSON.stringify({ query }),
+      });
+      return r.json();
+    },
+  };
+}
 
-    const traces = await getTraces();
-    expect(traces.data.length).toBe(3);
-
-    const relevantTraces = traces.data.filter((trace) =>
-      trace.spans.some((span) => span.operationName === 'POST /graphql'),
-    );
-    expect(relevantTraces.length).toBe(1);
-    const relevantTrace = relevantTraces[0]!;
-    expect(relevantTrace).toBeDefined();
-
-    // Check for extraction of the otel context
-    expect(relevantTrace.traceID).toBe(traceId);
-    for (const span of relevantTrace.spans) {
-      expect(span.traceID).toBe(traceId);
-    }
-
-    expect(upstreamHttpCalls.length).toBe(2);
-
-    for (const call of upstreamHttpCalls) {
-      if (call.headers?.['x-request-id']) {
-        const transparentHeader = (call.headers || {})['traceparent'];
-        expect(transparentHeader).toBeDefined();
-        expect(transparentHeader?.length).toBeGreaterThan(1);
-        expect(transparentHeader).toContain(traceId);
-      }
-    }
+it.skipIf(gatewayRunner !== 'node')('should execute', async () => {
+  const { output: supergraph } = await composeWithMesh({
+    output: 'ts',
+    services: [
+      await service('accounts'),
+      await service('inventory'),
+      await service('products'),
+      await service('reviews'),
+    ],
   });
+
+  await fs.copyFile(supergraph, path.join(__dirname, 'src', 'supergraph.ts'));
+
+  const { execute } = await wrangler();
+
+  await expect(
+    execute({
+      query: /* GraphQL */ `
+        fragment User on User {
+          id
+          username
+          name
+        }
+
+        fragment Review on Review {
+          id
+          body
+        }
+
+        fragment Product on Product {
+          inStock
+          name
+          price
+          shippingEstimate
+          upc
+          weight
+        }
+
+        query TestQuery {
+          users {
+            ...User
+            reviews {
+              ...Review
+              product {
+                ...Product
+                reviews {
+                  ...Review
+                  author {
+                    ...User
+                    reviews {
+                      ...Review
+                      product {
+                        ...Product
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          topProducts {
+            ...Product
+            reviews {
+              ...Review
+              author {
+                ...User
+                reviews {
+                  ...Review
+                  product {
+                    ...Product
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+    }),
+  ).resolves.toMatchSnapshot();
 });
--- e2e/cloudflare-workers/src/index.ts
+++ e2e/cloudflare-workers/src/index.ts
@@ -2,68 +2,23 @@ import { ExportedHandler, Response } from '@cloudflare/workers-types';
 import {
   createGatewayRuntime,
   DisposableSymbols,
-  GatewayPlugin,
 } from '@graphql-hive/gateway-runtime';
-import {
-  createOtlpHttpExporter,
-  useOpenTelemetry,
-} from '@graphql-mesh/plugin-opentelemetry';
 import http from '@graphql-mesh/transport-http';
+import rest from '@graphql-mesh/transport-rest';
 import { fakePromise } from '@graphql-tools/utils';
+// @ts-ignore
+import supergraph from './supergraph';
 
 interface Env {
-  OTLP_EXPORTER_URL: string;
-  OTLP_SERVICE_NAME: string;
   DEBUG: string;
 }
-
-const upstreamCallHeaders: Array<{
-  url: string;
-  headers?: HeadersInit;
-}> = [];
-
-// The following plugin is used to trace the fetch calls made by Mesh.
-const useOnFetchTracer = (): GatewayPlugin => {
-  return {
-    onFetch({ url, options }) {
-      upstreamCallHeaders.push({ url, headers: options.headers });
-    },
-    onRequest({ request, endResponse, fetchAPI }) {
-      if (request.url.includes('upstream-fetch') && request.method === 'GET') {
-        endResponse(fetchAPI.Response.json(upstreamCallHeaders));
-        return;
-      }
-    },
-  };
-};
 
 export default {
   async fetch(req, env, ctx) {
     const runtime = createGatewayRuntime({
-      proxy: {
-        endpoint: 'https://countries.trevorblades.com',
-      },
-      transports: {
-        http,
-      },
-      plugins: (ctx) => [
-        useOpenTelemetry({
-          ...ctx,
-          exporters: [
-            createOtlpHttpExporter(
-              {
-                url: env['OTLP_EXPORTER_URL'],
-              },
-              // Batching config is set in order to make it easier to test.
-              {
-                scheduledDelayMillis: 1,
-              },
-            ),
-          ],
-          serviceName: env['OTLP_SERVICE_NAME'],
-        }),
-        useOnFetchTracer(),
-      ],
+      supergraph,
+      transports: { http, rest },
+      maskedErrors: false,
     });
     const res = await runtime(req, env, ctx);
     ctx.waitUntil(fakePromise(runtime[DisposableSymbols.asyncDispose]()));
