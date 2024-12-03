b98f2a82c0818b8b0607ced67f09ce6892d700df
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-12-03 13:57:59 +0300

    Reproduction #136

---
+++ e2e/reproduction-136/mesh.config.ts
@@ -0,0 +1,21 @@
+import { createPrefixTransform, defineConfig, loadGraphQLHTTPSubgraph } from '@graphql-mesh/compose-cli';
+import { Opts } from '@internal/testing';
+
+const opts = Opts(process.argv);
+
+export const composeConfig = defineConfig({
+    subgraphs: [
+        {
+            sourceHandler: loadGraphQLHTTPSubgraph('my-subgraph', {
+                endpoint: `http://localhost:${opts.getServicePort('my-subgraph')}/graphql`,
+                source: './services/my-subgraph/schema.graphql',
+            }),
+            transforms: [
+                createPrefixTransform({
+                    value: 'test_',
+                    includeRootOperations: true,
+                }),
+            ],
+        },
+    ],
+});
