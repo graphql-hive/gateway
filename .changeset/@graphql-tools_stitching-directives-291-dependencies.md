34d12249ead65b8277df976f6318dca757df1151
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-12-06 16:07:35 +0300

    Update dependencies (#291)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .changeset/@graphql-tools_stitching-directives-291-dependencies.md
@@ -0,0 +1,7 @@
+---
+'@graphql-tools/stitching-directives': patch
+---
+
+dependencies updates: 
+
+- Updated dependency [`tslib@^2.8.1` ↗︎](https://www.npmjs.com/package/tslib/v/2.8.1) (from `^2.4.0`, in `dependencies`)
--- packages/fusion-runtime/scripts/replace-import-with-require.mjs
+++ packages/fusion-runtime/scripts/replace-import-with-require.mjs
@@ -2,5 +2,8 @@ import fs from 'node:fs';
 
 const cjsFile = './dist/index.cjs';
 const fileContent = fs.readFileSync(cjsFile, 'utf8');
-const newContent = fileContent.replace('import(kind)', 'require(kind)');
+const newContent = fileContent.replace(
+  'import(moduleName)',
+  'require(moduleName)',
+);
 fs.writeFileSync(cjsFile, newContent, 'utf8');
