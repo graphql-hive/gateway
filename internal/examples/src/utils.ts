9971019282cb6549a05312e2af7ed6973b67c09f
enisdenjo <denis@denelop.com>
2024-12-24 17:48:20 +0100

    log a lot

--- internal/examples/src/utils.ts
+++ internal/examples/src/utils.ts
@@ -1,4 +1,5 @@
 import fs from 'node:fs/promises';
+import { ASTPath } from 'jscodeshift';
 
 /** Checks whether a file exists at the given {@link path}. */
 export function exists(path: string) {
@@ -6,4 +7,25 @@ export function exists(path: string) {
     .stat(path)
     .then(() => true)
     .catch(() => false);
+}
+
+export function defer(cb: () => void) {
+  return {
+    [Symbol.dispose]: cb,
+  };
+}
+
+/** Gets the Line Of Code numbers (with optional column number) in format `L0:0`. */
+export function loc({ node }: ASTPath, includeColumn?: true) {
+  if (!('loc' in node)) {
+    throw new Error('Node does not have a location in source');
+  }
+  if (!node.loc) {
+    throw new Error('Node location in source is empty');
+  }
+  let str = `L${node.loc.start.line}`;
+  if (includeColumn) {
+    str += `:${node.loc.start.column}`;
+  }
+  return str;
 }
