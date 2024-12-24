530d180fc1e338087412482ba366490e4ec9a14d
enisdenjo <denis@denelop.com>
2024-12-24 17:26:45 +0100

    transform mesh config

--- internal/examples/src/convert.ts
+++ internal/examples/src/convert.ts
@@ -26,69 +26,74 @@ export async function convertE2EToExample(config: ConvertE2EToExampleConfig) {
 
   const meshConfigTsFile = path.join(config.e2eDir, 'mesh.config.ts');
   if (await exists(meshConfigTsFile)) {
-    console.log(`"mesh.config.ts" found, converting...`);
-
-    const root = j(await fs.readFile(meshConfigTsFile, 'utf8'));
-
-    root
-      // import '@internal/testing'
-      .find(j.ImportDeclaration, {
-        source: {
-          value: '@internal/testing',
-        },
-      })
-      .forEach((p) => {
-        p.node.specifiers
-          // import { Opts } from '@internal/testing'
-          ?.filter((s) => 'imported' in s && s.imported.name === 'Opts')
-          .forEach((s) => {
-            root
-              // const opts = Opts()
-              .find(j.VariableDeclarator, {
-                init: {
-                  callee: {
-                    name: s.local!.name,
-                  },
-                },
-              })
-              .forEach((p) => {
-                if (p.node.id.type !== 'Identifier') return;
-
-                const startingServicePort = 4001;
-                const servicePort: { [service: string]: number /* port */ } =
-                  {};
-
-                root
-                  // opts.getServicePort()
-                  .find(j.CallExpression, {
-                    callee: {
-                      object: {
-                        name: p.node.id.name,
-                      },
-                      property: {
-                        name: 'getServicePort',
-                      },
-                    },
-                  })
-                  .forEach((p, i) => {
-                    const arg0 = p.node.arguments[0];
-                    if (arg0?.type !== 'Literal') {
-                      throw new Error(
-                        'TODO: get variable value when literal is not used in "opts.getServicePort" argument',
-                      );
-                    }
-
-                    const port = startingServicePort + i;
-                    servicePort[arg0.value!.toString()] = port;
-
-                    j(p).replaceWith(j.literal(port)); // replace opts.servicePort('foo') with port literal
-                  });
-              })
-              .remove(); // remove all const opts = Opts()
-          });
-      })
-      .remove(); // remove all import '@internal/testing'
-
-    console.log(root.toSource());
+    console.log(`"mesh.config.ts" found, transforming...`);
+    transformMeshConfigFile(await fs.readFile(meshConfigTsFile, 'utf8'));
   }
+}
+
+/**
+ * @param source - Source code of the `mesh.config.ts` file.
+ */
+function transformMeshConfigFile(source: string) {
+  const root = j(source);
+
+  const startingServicePort = 4001;
+  const portForService: { [service: string]: number /* port */ } = {};
+
+  root
+    // import '@internal/testing'
+    .find(j.ImportDeclaration, {
+      source: {
+        value: '@internal/testing',
+      },
+    })
+    .forEach((p) => {
+      p.node.specifiers
+        // import { Opts } from '@internal/testing'
+        ?.filter((s) => 'imported' in s && s.imported.name === 'Opts')
+        .forEach((s) => {
+          root
+            // const opts = Opts()
+            .find(j.VariableDeclarator, {
+              init: {
+                callee: {
+                  name: s.local!.name,
+                },
+              },
+            })
+            .forEach((p) => {
+              if (p.node.id.type !== 'Identifier') return;
+
+              root
+                // opts.getServicePort()
+                .find(j.CallExpression, {
+                  callee: {
+                    object: {
+                      name: p.node.id.name,
+                    },
+                    property: {
+                      name: 'getServicePort',
+                    },
+                  },
+                })
+                .forEach((p, i) => {
+                  const arg0 = p.node.arguments[0];
+                  if (arg0?.type !== 'Literal') {
+                    throw new Error(
+                      'TODO: get variable value when literal is not used in "opts.getServicePort" argument',
+                    );
+                  }
+
+                  const port = startingServicePort + i;
+                  portForService[arg0.value!.toString()] = port;
+
+                  j(p).replaceWith(j.literal(port)); // replace opts.portForService('foo') with port literal
+                });
+            })
+            .remove(); // remove all const opts = Opts()
+        });
+    })
+    .remove(); // remove all import '@internal/testing'
+
+  return { source: root.toSource(), portForService };
 }
