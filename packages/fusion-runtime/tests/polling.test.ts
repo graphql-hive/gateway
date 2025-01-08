356675f9ebf18639bf95b440779bfaa6f579fc4e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-01-08 14:47:36 +0300

    fix: dispose the executor only if the schema has changed

--- packages/fusion-runtime/tests/polling.test.ts
+++ packages/fusion-runtime/tests/polling.test.ts
@@ -1,13 +1,16 @@
 import { setTimeout } from 'timers/promises';
 import { getUnifiedGraphGracefully } from '@graphql-mesh/fusion-composition';
+import { getExecutorForUnifiedGraph } from '@graphql-mesh/fusion-runtime';
 import {
   createDefaultExecutor,
   type DisposableExecutor,
 } from '@graphql-mesh/transport-common';
+import { DefaultLogger, LogLevel, makeDisposable } from '@graphql-mesh/utils';
 import { normalizedExecutor } from '@graphql-tools/executor';
-import { isAsyncIterable } from '@graphql-tools/utils';
+import { fakePromise, isAsyncIterable } from '@graphql-tools/utils';
+import { assertSingleExecutionValue } from '@internal/testing';
 import { DisposableSymbols } from '@whatwg-node/disposablestack';
-import { GraphQLSchema, parse } from 'graphql';
+import { ExecutionResult, GraphQLSchema, parse } from 'graphql';
 import { createSchema } from 'graphql-yoga';
 import { describe, expect, it, vi } from 'vitest';
 import { UnifiedGraphManager } from '../src/unifiedGraphManager';
@@ -200,7 +203,7 @@ describe('Polling', () => {
     }
     try {
       await manager.getUnifiedGraph();
-      await expect(true).toBeFalsy();
+      expect(true).toBeFalsy();
     } catch (e) {
       // Ignore
     }
@@ -214,5 +217,91 @@ describe('Polling', () => {
     await advanceTimersByTimeAsync(pollingInterval);
     // Should keep polling even if it fails in somewhere
     expect(unifiedGraphFetcher).toHaveBeenCalledTimes(4);
+  });
+  it('does not stop request if the polled schema is not changed', async () => {
+    vi.useFakeTimers?.();
+    const pollingInterval = 1000;
+    const requestDuration = 10_000;
+    const schema = createSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          greetings: String
+        }
+      `,
+      resolvers: {
+        Query: {
+          greetings() {
+            return new Promise<string>((resolve) => {
+              globalThis.setTimeout(() => {
+                resolve('Hello');
+              }, requestDuration);
+            });
+          },
+        },
+      },
+    });
+    const callTimes: number[] = [];
+    const startTime = Date.now();
+    const unifiedGraphFetcher = vi.fn(() => {
+      callTimes.push(Date.now() - startTime);
+      return getUnifiedGraphGracefully([
+        {
+          name: 'Test',
+          schema,
+        },
+      ]);
+    });
+    let disposeFn = vi.fn();
+    await using executor = getExecutorForUnifiedGraph({
+      getUnifiedGraph: unifiedGraphFetcher,
+      pollingInterval,
+      transportContext: {
+        logger: new DefaultLogger('_', LogLevel.debug),
+      },
+      transports() {
+        return {
+          getSubgraphExecutor() {
+            return makeDisposable(createDefaultExecutor(schema), disposeFn);
+          },
+        };
+      },
+    });
+    let result: ExecutionResult | undefined;
+    let err: Error | undefined;
+    fakePromise(
+      executor({
+        document: parse(/* GraphQL */ `
+          query {
+            greetings
+          }
+        `),
+      }),
+    ).then(
+      (r) => {
+        assertSingleExecutionValue(r);
+        result = r;
+        return r;
+      },
+      (e) => {
+        err = e;
+      },
+    );
+    let totalTimeLeft = requestDuration;
+    await advanceTimersByTimeAsync(pollingInterval * 2);
+    totalTimeLeft -= pollingInterval * 2;
+    // After twice polling interval, the request should be still pending
+    expect(result).toBeUndefined();
+    expect(err).toBeUndefined();
+    expect(callTimes).toEqual([0, pollingInterval, pollingInterval * 2]);
+    await advanceTimersByTimeAsync(totalTimeLeft);
+    expect(result).toEqual({
+      data: {
+        greetings: 'Hello',
+      },
+    });
+    expect(disposeFn).toHaveBeenCalledTimes(0);
+    expect(callTimes).toHaveLength(
+      Math.ceil(requestDuration / pollingInterval) + 1,
+    );
   });
 });
