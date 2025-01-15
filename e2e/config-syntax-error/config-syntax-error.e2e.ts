4ccb1350f73252b1299b86e510deb5656e21ea9d
Denis Badurina <denis@denelop.com>
2025-01-15 19:15:32 +0100

    expect fail

--- e2e/config-syntax-error/config-syntax-error.e2e.ts
+++ e2e/config-syntax-error/config-syntax-error.e2e.ts
@@ -3,29 +3,15 @@ import { expect, it } from 'vitest';
 
 const { gateway, service } = createTenv(__dirname);
 
-it('should execute query from additional resolvers', async () => {
-  const { execute } = await gateway({
-    supergraph: {
-      with: 'mesh',
-      services: [await service('hello')],
-    },
-  });
-
+it('should point to exact location of syntax error when parsing a malformed config', async () => {
   await expect(
-    execute({
-      query: /* GraphQL */ `
-        {
-          hello
-          bye
-        }
-      `,
-    }),
-  ).resolves.toMatchInlineSnapshot(`
-    {
-      "data": {
-        "bye": "world",
-        "hello": "world",
+    gateway({
+      supergraph: {
+        with: 'mesh',
+        services: [await service('hello')],
       },
-    }
-  `);
+    }),
+  ).rejects.toThrowError(
+    /SyntaxError \[Error\]: Error transforming (.*)\/custom-resolvers.ts: Unexpected token, expected "{" \(8:11\)/,
+  );
 });
