d10e9b7c401b958136998be7293e59b3e0af5c1a
Denis Badurina <denis@denelop.com>
2025-01-16 17:30:59 +0100

    debug...

--- e2e/config-syntax-error/config-syntax-error.e2e.ts
+++ e2e/config-syntax-error/config-syntax-error.e2e.ts
@@ -1,29 +1,47 @@
+// @ts-nocheck
+
 import { createTenv } from '@internal/e2e';
 import { expect, it } from 'vitest';
 
 const { gateway, service, gatewayRunner } = createTenv(__dirname);
 
 it('should point to exact location of syntax error when parsing a malformed config', async () => {
-  await expect(
-    gateway({
-      supergraph: {
-        with: 'mesh',
-        services: [await service('hello')],
+  await gateway({
+    supergraph: {
+      with: 'mesh',
+      services: [await service('hello')],
+    },
+    runner: {
+      docker: {
+        volumes: [
+          {
+            host: 'custom-resolvers.ts',
+            container: '/gateway/custom-resolvers.ts',
+          },
+        ],
       },
-      runner: {
-        docker: {
-          volumes: [
-            {
-              host: 'custom-resolvers.ts',
-              container: '/gateway/custom-resolvers.ts',
-            },
-          ],
-        },
-      },
-    }),
-  ).rejects.toThrowError(
-    gatewayRunner === 'bun' || gatewayRunner === 'bun-docker'
-      ? /error: Expected "{" but found "hello"(.|\n)*\/custom-resolvers.ts:8:11/
-      : /SyntaxError \[Error\]: Error transforming .*(\/|\\)custom-resolvers.ts: Unexpected token, expected "{" \(8:11\)/,
-  );
+    },
+  });
+  // await expect(
+  //   gateway({
+  //     supergraph: {
+  //       with: 'mesh',
+  //       services: [await service('hello')],
+  //     },
+  //     runner: {
+  //       docker: {
+  //         volumes: [
+  //           {
+  //             host: 'custom-resolvers.ts',
+  //             container: '/gateway/custom-resolvers.ts',
+  //           },
+  //         ],
+  //       },
+  //     },
+  //   }),
+  // ).rejects.toThrowError(
+  //   gatewayRunner === 'bun' || gatewayRunner === 'bun-docker'
+  //     ? /error: Expected "{" but found "hello"(.|\n)*\/custom-resolvers.ts:8:11/
+  //     : /SyntaxError \[Error\]: Error transforming .*(\/|\\)custom-resolvers.ts: Unexpected token, expected "{" \(8:11\)/,
+  // );
 });
