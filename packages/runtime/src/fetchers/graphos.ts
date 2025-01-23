3b1cabb0faeeadc092ca6205851e71f7cb8fd55f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-01-23 14:14:27 +0300

    More

--- packages/runtime/src/fetchers/graphos.ts
+++ packages/runtime/src/fetchers/graphos.ts
@@ -2,11 +2,11 @@ import {
   DEFAULT_UPLINKS,
   fetchSupergraphSdlFromManagedFederation,
 } from '@graphql-tools/federation';
-import { mapMaybePromise, type MaybePromise } from '@graphql-tools/utils';
 import type {
   GatewayConfigContext,
   GatewayGraphOSManagedFederationOptions,
 } from '../index';
+import { delayInMs } from '../utils';
 
 export interface CreateGraphOSFetcherOpts {
   graphosOpts: GatewayGraphOSManagedFederationOptions;
@@ -40,100 +40,84 @@ export function createGraphOSFetcher({
     supergraphLoadedPlace,
     unifiedGraphFetcher() {
       const uplinksToUse: string[] = [];
-      let retries = graphosOpts.maxRetries || Math.max(3, uplinks.length);
-      const fetchSupergraphWithDelay = (): MaybePromise<string> => {
+      let retries = maxRetries;
+      function fetchSupergraphWithDelay(): Promise<string> {
         if (nextFetchTime) {
           const currentTime = Date.now();
           if (nextFetchTime >= currentTime) {
             const delay = nextFetchTime - currentTime;
             graphosLogger.info(`Fetching supergraph with delay: ${delay}ms`);
-            return new Promise((resolve) =>
-              setTimeout(() => {
-                nextFetchTime = 0;
-                resolve(fetchSupergraph());
-              }, delay),
-            );
+            nextFetchTime = 0;
+            return delayInMs(delay).then(fetchSupergraph);
           }
         }
         return fetchSupergraph();
-      };
-      const fetchSupergraph = (): MaybePromise<string> => {
+      }
+      function fetchSupergraph(): Promise<string> {
         if (uplinksToUse.length === 0) {
           uplinksToUse.push(...uplinks);
         }
         retries--;
-        try {
-          const uplinkToUse = uplinksToUse.pop();
-          const attemptLogger = graphosLogger.child(
-            `Attempt ${maxRetries - retries} - UpLink: ${uplinkToUse}`,
-          );
-          attemptLogger.debug(`Fetching supergraph`);
-          return mapMaybePromise(
-            fetchSupergraphSdlFromManagedFederation({
-              graphRef: graphosOpts.graphRef,
-              apiKey: graphosOpts.apiKey,
-              upLink: uplinkToUse,
-              lastSeenId,
-              // @ts-expect-error TODO: what's up with type narrowing
-              fetch: configContext.fetch,
-              loggerByMessageLevel: {
-                ERROR(message) {
-                  attemptLogger.error(message);
-                },
-                INFO(message) {
-                  attemptLogger.info(message);
-                },
-                WARN(message) {
-                  attemptLogger.warn(message);
-                },
-              },
-            }),
-            (result) => {
-              if (result.minDelaySeconds) {
-                attemptLogger.debug(
-                  `Setting min delay to ${result.minDelaySeconds}s`,
-                );
-                nextFetchTime = Date.now() + result.minDelaySeconds * 1000;
-              }
-              if ('error' in result) {
-                attemptLogger.error(result.error.code, result.error.message);
-                if (retries > 0) {
-                  return fetchSupergraphWithDelay();
-                }
-              }
-              if ('id' in result) {
-                lastSeenId = result.id;
-              }
-              if ('supergraphSdl' in result) {
-                attemptLogger.info(
-                  `Fetched the new supergraph ${lastSeenId ? `with id ${lastSeenId}` : ''}`,
-                );
-                lastSupergraphSdl = result.supergraphSdl;
-              }
-              if (!lastSupergraphSdl) {
-                if (retries > 0) {
-                  return fetchSupergraphWithDelay();
-                }
-                throw new Error('Failed to fetch supergraph SDL');
-              }
-              return lastSupergraphSdl;
+        const uplinkToUse = uplinksToUse.pop();
+        const attemptLogger = graphosLogger.child(
+          `Attempt ${maxRetries - retries} - UpLink: ${uplinkToUse}`,
+        );
+        attemptLogger.debug(`Fetching supergraph`);
+        return fetchSupergraphSdlFromManagedFederation({
+          graphRef: graphosOpts.graphRef,
+          apiKey: graphosOpts.apiKey,
+          upLink: uplinkToUse,
+          lastSeenId,
+          // @ts-expect-error TODO: what's up with type narrowing
+          fetch: configContext.fetch,
+          loggerByMessageLevel: {
+            ERROR(message) {
+              attemptLogger.error(message);
             },
-            (err) => {
-              configContext.logger.child('GraphOS').error(err);
+            INFO(message) {
+              attemptLogger.info(message);
+            },
+            WARN(message) {
+              attemptLogger.warn(message);
+            },
+          },
+        }).then(
+          (result) => {
+            if (result.minDelaySeconds) {
+              attemptLogger.debug(
+                `Setting min delay to ${result.minDelaySeconds}s`,
+              );
+              nextFetchTime = Date.now() + result.minDelaySeconds * 1000;
+            }
+            if ('error' in result) {
+              attemptLogger.error(result.error.code, result.error.message);
+            }
+            if ('id' in result) {
+              lastSeenId = result.id;
+            }
+            if ('supergraphSdl' in result) {
+              attemptLogger.info(
+                `Fetched the new supergraph ${lastSeenId ? `with id ${lastSeenId}` : ''}`,
+              );
+              lastSupergraphSdl = result.supergraphSdl;
+            }
+            if (!lastSupergraphSdl) {
               if (retries > 0) {
                 return fetchSupergraphWithDelay();
               }
-              return lastSupergraphSdl;
-            },
-          );
-        } catch (e) {
-          configContext.logger.child('GraphOS').error(e);
-          if (retries > 0) {
-            return fetchSupergraphWithDelay();
-          }
-          return lastSupergraphSdl;
-        }
-      };
+              throw new Error('Failed to fetch supergraph SDL');
+            }
+            return lastSupergraphSdl;
+          },
+          (err) => {
+            graphosLogger.error(err);
+            if (retries > 0) {
+              return fetchSupergraphWithDelay();
+            }
+            return lastSupergraphSdl;
+          },
+        );
+      }
       return fetchSupergraphWithDelay();
     },
   };
