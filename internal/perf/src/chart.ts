d5cba610a6ed467b03cdef0abec31949a013ecf7
Denis Badurina <denis@denelop.com>
2025-02-10 16:05:20 +0100

    memory usage chart in debug mode

---
+++ internal/perf/src/chart.ts
@@ -0,0 +1,66 @@
+import { Canvas, createCanvas } from 'canvas';
+import { Chart, ChartConfiguration } from 'chart.js/auto';
+
+export interface LineChartDataset {
+  /** The label of the line in the line chart. */
+  label: string;
+  /** The X data points in the line chart, which are the labels. */
+  x: (number | string)[];
+  /** The Y data points in the line chart. */
+  y: number[];
+}
+
+export interface LineChartOptions {
+  /**
+   * The tick label callbacks of {@link LineChartDataset.y y} entries.
+   *
+   * TODO: separate the tick callback for each data.
+   */
+  yTicksCallback?: (tickValue: number | string) => string;
+}
+
+export function createLineChart(
+  datasets: LineChartDataset[],
+  options: LineChartOptions = {},
+): Canvas {
+  const canvas = createCanvas(800, 400, 'svg');
+
+  const chartConfig: ChartConfiguration = {
+    type: 'line',
+    data: {
+      labels: datasets.map(({ x }) => x),
+      datasets: datasets.map(({ label, y }) => ({
+        label,
+        data: y,
+      })),
+    },
+    options: {
+      responsive: false, // because we're rendering the chart statically
+      scales: {
+        y: {
+          ticks: {
+            callback: options.yTicksCallback,
+          },
+        },
+      },
+    },
+    plugins: [
+      {
+        id: 'set-white-background',
+        beforeDraw: (chart) => {
+          chart.ctx.fillStyle = 'white';
+          chart.ctx.fillRect(0, 0, chart.width, chart.height);
+          chart.ctx.restore();
+        },
+      },
+    ],
+  };
+
+  new Chart(
+    // @ts-expect-error canvas types are of a different instance, but they fit
+    canvas.getContext('2d'),
+    chartConfig,
+  );
+
+  return canvas;
+}
