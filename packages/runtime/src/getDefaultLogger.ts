521ec452d6ffa5df55c3f6b7097ec33f8c4e6336
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-02-12 18:03:52 +0300

    Lets go

--- packages/runtime/src/getDefaultLogger.ts
+++ packages/runtime/src/getDefaultLogger.ts
@@ -1,9 +1,6 @@
-// eslint-disable-next-line import/no-nodejs-modules
-import { Console } from 'node:console';
 import { JSONLogger } from '@graphql-hive/logger-json';
 import { process } from '@graphql-mesh/cross-helpers';
-import { LogLevel } from '@graphql-mesh/utils';
-import pinoPretty from 'pino-pretty';
+import { DefaultLogger, LogLevel } from '@graphql-mesh/utils';
 
 export function getDefaultLogger(opts?: { name?: string; level?: LogLevel }) {
   const logFormat = process.env['LOG_FORMAT'] || (globalThis as any).LOG_FORMAT;
@@ -11,29 +8,12 @@ export function getDefaultLogger(opts?: { name?: string; level?: LogLevel }) {
     if (logFormat.toLowerCase() === 'json') {
       return new JSONLogger(opts);
     } else if (logFormat.toLowerCase() === 'pretty') {
-      return createPrettyLogger(opts);
+      return new DefaultLogger(opts?.name, opts?.level);
     }
   }
   const nodeEnv = process.env['NODE_ENV'] || (globalThis as any).NODE_ENV;
   if (nodeEnv === 'production') {
     return new JSONLogger(opts);
   }
-  return createPrettyLogger(opts);
-}
-
-function createPrettyLogger(opts?: { name?: string; level?: LogLevel }) {
-  const stdOut = pinoPretty({
-    levelFirst: true,
-    colorize: true,
-    destination: process.stdout,
-  });
-  const stdErr = pinoPretty({
-    levelFirst: true,
-    colorize: true,
-    destination: process.stdout,
-  });
-  return new JSONLogger({
-    ...opts,
-    console: new Console(stdOut, stdErr),
-  });
+  return new DefaultLogger(opts?.name, opts?.level);
 }
