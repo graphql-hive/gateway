a9da80c34b597e38bef2a00d02fadafa9c001eb2
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-02-12 20:50:07 +0300

    ..

--- packages/gateway/src/commands/handleFork.ts
+++ packages/gateway/src/commands/handleFork.ts
@@ -12,30 +12,32 @@ export function handleFork(log: Logger, config: { fork?: number }): boolean {
         const worker = cluster.fork();
         const workerLogger = log.child({ worker: worker.id });
         worker.once('exit', (code, signal) => {
+          const logData: Record<string, string | number> = {
+            signal,
+          };
+          if (code != null) {
+            logData['code'] = code;
+          }
           if (expectedToExit) {
-            workerLogger.debug('exited', {
-              code,
-              signal,
-            });
+            workerLogger.debug('exited', logData);
           } else {
             workerLogger.error(
               'exited unexpectedly. A restart is recommended to ensure the stability of the service',
-              {
-                code,
-                signal,
-              },
+              logData,
             );
           }
           workers.delete(worker);
           if (!expectedToExit && workers.size === 0) {
-            log.error(`All workers exited unexpectedly. Exiting`);
+            log.error(`All workers exited unexpectedly. Exiting`, logData);
             process.exit(1);
           }
         });
         workers.add(worker);
       }
       registerTerminateHandler((signal) => {
-        log.info(`Killing workers with ${signal}`);
+        log.info('Killing workers', {
+          signal,
+        });
         expectedToExit = true;
         workers.forEach((w) => {
           w.kill(signal);
