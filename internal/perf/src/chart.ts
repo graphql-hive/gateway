5f77a43d453e3004dfe1efc4f4b8bbd39025e4c4
Denis Badurina <denis@denelop.com>
2025-02-28 20:15:54 +0100

    less trend and stuff

--- internal/perf/src/chart.ts
+++ internal/perf/src/chart.ts
@@ -93,6 +93,7 @@ export async function createMemorySampleLineChart(
             label: {
               color: 'black',
               text: 'Memory trend',
+              displayValue: false,
             },
           },
           segment: {
