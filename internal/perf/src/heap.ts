c51ed2bdd1ce6f8584752c4b3c545b2a765ac5c8
Denis Badurina <denis@denelop.com>
2025-03-06 17:57:16 +0100

    analyse frames and report

--- internal/perf/src/heap.ts
+++ internal/perf/src/heap.ts
@@ -1,7 +1,7 @@
 import { HeapProfiler } from 'inspector';
 import { serializer } from '@memlab/core';
 import { getFullHeapFromFile, PluginUtils } from '@memlab/heap-analysis';
-import { Frame } from 'speedscope/profile';
+import { CallTreeNode, Frame } from 'speedscope/profile';
 import { importFromChromeHeapProfile } from 'speedscope/profile/v8heapalloc';
 
 export async function analyzeHeapSnapshot(file: string) {
@@ -90,18 +90,91 @@ export async function analyzeHeapSnapshot(file: string) {
   console.groupEnd();
 }
 
-export async function analyzeHeapSamplingProfile(
+export interface HeapSamplingProfileNode {
+  name: string;
+  file: string | null;
+  /** The size in memory the frame itself allocated in bytes. */
+  selfSize: number;
+  /** {@link selfSize Self size} in MBs. */
+  selfSizeInMB: number;
+  /** The size in memory the frame and its callees allocated in bytes. */
+  totalSize: number;
+  /** {@link totalSize Total size} in MBs. */
+  totalSizeInMB: number;
+}
+
+export interface HeapSamplingProfileFrame extends HeapSamplingProfileNode {
+  /** Callstack of heaviest frames ordered from root the leaf (this frame). */
+  callstack: HeapSamplingProfileNode[];
+}
+
+/**
+ * Analyses the {@link v8Profile v8 heap sampling profile}.
+ *
+ * @returns The heaviest leaf frames.
+ */
+export function analyzeHeapSamplingProfile(
   v8Profile: HeapProfiler.SamplingHeapProfile,
-) {
+): HeapSamplingProfileFrame[] {
   const profile = importFromChromeHeapProfile(
     // @ts-expect-error is ok, speedscope will add the id and totalSize properties
     v8Profile,
   );
 
-  const frames: Frame[] = [];
+  // frames that on their own have allocated a lot of memory out of all memory
+  const totalSize = profile.getTotalNonIdleWeight();
+  const highSelfSizeFrames: Frame[] = [];
+  profile.forEachFrame((frame) => {
+    const selfPerc = frame.getSelfWeight() / totalSize;
+    if (selfPerc > 0.1) {
+      // self sizes taking up higher than 10% of the whole profile are considered big
+      highSelfSizeFrames.push(frame);
+    }
+  });
+  highSelfSizeFrames.sort((a, b) => b.getSelfWeight() - a.getSelfWeight());
 
-  profile.forEachFrame((f) => frames.push(f));
-  frames.sort((a, b) => b.getSelfWeight() - a.getSelfWeight());
+  const heaviestFrames: HeapSamplingProfileFrame[] = [];
 
-  // TODO: analyse the frames for leak detection
+  for (const frame of highSelfSizeFrames) {
+    // get the callers of this frame (this frame is the leaf node)
+    const callers = profile.getInvertedProfileForCallersOf(frame);
+
+    // "grouped" call tree is one in which each node has at most one child per
+    // frame. nodes are ordered in decreasing order of weight
+    const calltree = callers.getGroupedCalltreeRoot();
+
+    // the call stack with the biggest size from the root to the leaf node (this frame)
+    const biggestWeightStack: Frame[] = [];
+    let node: CallTreeNode | undefined = calltree.children[0];
+    while (node) {
+      biggestWeightStack.push(node.frame);
+      node = node.children[0];
+    }
+    biggestWeightStack.reverse();
+
+    heaviestFrames.push({
+      name: frame.name,
+      file: frame.file || null,
+      selfSize: frame.getSelfWeight(),
+      selfSizeInMB: Number((frame.getSelfWeight() / (1024 * 1024)).toFixed(2)),
+      totalSize: frame.getTotalWeight(),
+      totalSizeInMB: Number(
+        (frame.getTotalWeight() / (1024 * 1024)).toFixed(2),
+      ),
+      callstack: biggestWeightStack.map((frame) => ({
+        name: frame.name,
+        file: frame.file || null,
+        selfSize: frame.getSelfWeight(),
+        selfSizeInMB: Number(
+          (frame.getSelfWeight() / (1024 * 1024)).toFixed(2),
+        ),
+        totalSize: frame.getTotalWeight(),
+        totalSizeInMB: Number(
+          (frame.getTotalWeight() / (1024 * 1024)).toFixed(2),
+        ),
+      })),
+    });
+  }
+
+  return heaviestFrames;
 }
