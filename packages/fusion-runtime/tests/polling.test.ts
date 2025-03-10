c850edb08a9026b446e70710903f974c26c27961
Valentin Cocaud <v.cocaud@gmail.com>
2025-03-10 12:10:31 +0100

    Reproduction of failing Polling tests when executor dispose is actually async

--- packages/fusion-runtime/tests/polling.test.ts
+++ packages/fusion-runtime/tests/polling.test.ts
@@ -22,11 +22,14 @@ import { getDefaultLogger } from '../../runtime/src/getDefaultLogger';
 import { UnifiedGraphManager } from '../src/unifiedGraphManager';
 
 describe('Polling', () => {
-  const advanceTimersByTimeAsync = vi.advanceTimersByTimeAsync || setTimeout;
+  // const advanceTimersByTimeAsync = vi.advanceTimersByTimeAsync || setTimeout;
+  const advanceTimersByTimeAsync = setTimeout;
   it('polls the schema in a certain interval', async () => {
-    vi.useFakeTimers?.();
+    // vi.useFakeTimers?.();
+
     const pollingInterval = 300;
     let schema: GraphQLSchema;
+
     const unifiedGraphFetcher = () => {
       const time = new Date().toISOString();
       schema = createSchema({
@@ -53,7 +56,9 @@ describe('Polling', () => {
         },
       ]);
     };
-    const disposeFn = vi.fn();
+
+    const disposeFn = vi.fn().mockResolvedValue(undefined);
+
     await using manager = new UnifiedGraphManager({
       getUnifiedGraph: unifiedGraphFetcher,
       pollingInterval: pollingInterval,
@@ -70,6 +75,7 @@ describe('Polling', () => {
         };
       },
     });
+
     async function getFetchedTimeOnComment() {
       const schema = await manager.getUnifiedGraph();
       const queryType = schema.getQueryType();
@@ -81,6 +87,7 @@ describe('Polling', () => {
       const lastFetchedDate = new Date(lastFetchedDateStr);
       return lastFetchedDate;
     }
+
     async function getFetchedTimeFromResolvers() {
       const schema = await manager.getUnifiedGraph();
       const result = await normalizedExecutor({
@@ -96,25 +103,32 @@ describe('Polling', () => {
       }
       return new Date(result.data.time);
     }
+
     async function compareTimes() {
       const timeFromComment = await getFetchedTimeOnComment();
       const timeFromResolvers = await getFetchedTimeFromResolvers();
       expect(timeFromComment).toEqual(timeFromResolvers);
     }
+
     await compareTimes();
     const firstDate = await getFetchedTimeOnComment();
+
     await advanceTimersByTimeAsync(pollingInterval);
     await compareTimes();
     const secondDate = await getFetchedTimeOnComment();
+
     const diffBetweenFirstAndSecond =
       secondDate.getTime() - firstDate.getTime();
     expect(diffBetweenFirstAndSecond).toBeGreaterThanOrEqual(pollingInterval);
+
     await advanceTimersByTimeAsync(pollingInterval);
     await compareTimes();
     const thirdDate = await getFetchedTimeOnComment();
+
     const diffBetweenSecondAndThird =
       thirdDate.getTime() - secondDate.getTime();
     expect(diffBetweenSecondAndThird).toBeGreaterThanOrEqual(pollingInterval);
+
     const diffBetweenFirstAndThird = thirdDate.getTime() - firstDate.getTime();
     expect(diffBetweenFirstAndThird).toBeGreaterThanOrEqual(
       pollingInterval * 2,
