71b444b3782081ff3e37b5ef0e27ced5e0e882b0
Denis Badurina <denis@denelop.com>
2025-03-11 12:34:07 +0100

    handle reporting config considers the config

--- packages/gateway/src/commands/handleReportingConfig.ts
+++ packages/gateway/src/commands/handleReportingConfig.ts
@@ -5,64 +5,83 @@ import {
   GatewayHiveReportingOptions,
 } from '..';
 
+export interface ReportingCLIOptions {
+  hiveRegistryToken: string | undefined;
+  hiveUsageTarget: string | undefined;
+  hiveUsageAccessToken: string | undefined;
+  apolloGraphRef: string | undefined;
+  apolloKey: string | undefined;
+}
+
 export function handleReportingConfig(
   ctx: CLIContext,
   loadedConfig: Partial<GatewayConfig<Record<string, any>>>,
-  opts: {
-    hiveRegistryToken: string | undefined;
-    hiveUsageTarget: string | undefined;
-    hiveUsageAccessToken: string | undefined;
-    apolloGraphRef: string | undefined;
-    apolloKey: string | undefined;
-  },
+  cliOpts: ReportingCLIOptions,
 ): GatewayHiveReportingOptions | GatewayGraphOSReportingOptions | null {
-  const {
-    hiveRegistryToken,
-    hiveUsageTarget,
-    hiveUsageAccessToken,
-    apolloGraphRef,
-    apolloKey,
-  } = opts;
+  const confOpts: Partial<ReportingCLIOptions> = {
+    ...(loadedConfig.reporting?.type === 'hive'
+      ? {
+          hiveRegistryToken: loadedConfig.reporting.token,
+          hiveUsageTarget: loadedConfig.reporting.target,
+          hiveUsageAccessToken: loadedConfig.reporting.token,
+        }
+      : {}),
+    ...(loadedConfig.reporting?.type === 'graphos'
+      ? {
+          apolloGraphRef: loadedConfig.reporting.graphRef,
+          apolloKey: loadedConfig.reporting.apiKey,
+        }
+      : {}),
+  };
+  const opts = { ...confOpts, ...cliOpts };
 
-  if (hiveRegistryToken) {
-    if (hiveUsageAccessToken || hiveUsageTarget) {
-      ctx.log.error(
-        `Cannot use "--hive-registry-token" with "--hive-usage-target" or "--hive-usage-access-token". Please use "--hive-usage-target" and "--hive-usage-access-token" instead.`,
-      );
-      process.exit(1);
-    }
-    ctx.log.warn(
-      `"--hive-registry-token" is deprecated! Please use "--hive-usage-target" and "--hive-usage-access-token" instead.`,
+  if (cliOpts.hiveRegistryToken && cliOpts.hiveUsageAccessToken) {
+    ctx.log.error(
+      `Cannot use "--hive-registry-token" with "--hive-usage-access-token". Please use "--hive-usage-target" and "--hive-usage-access-token" or the config instead.`,
     );
-    ctx.log.info(`Configuring Hive registry reporting`);
-    return {
-      ...loadedConfig.reporting,
-      type: 'hive',
-      token: hiveRegistryToken,
-    };
-  } else if (hiveUsageAccessToken || hiveUsageTarget) {
-    if (!hiveUsageAccessToken) {
-      ctx.log.error(
-        `Hive usage target needs an access token. Please provide it through the "--hive-usage-access-token <token>" option or the config.`,
-      );
-      process.exit(1);
+    process.exit(1);
+  }
+
+  if (cliOpts.hiveRegistryToken && opts.hiveUsageTarget) {
+    ctx.log.error(
+      `Cannot use "--hive-registry-token" with a target. Please use "--hive-usage-target" and "--hive-usage-access-token" or the config instead.`,
+    );
+    process.exit(1);
+  }
+
+  if (opts.hiveUsageTarget && !opts.hiveUsageAccessToken) {
+    ctx.log.error(
+      `Hive usage target needs an access token. Please provide it through the "--hive-usage-access-token <token>" option or the config.`,
+    );
+    process.exit(1);
+  }
+
+  if (opts.hiveUsageAccessToken && !opts.hiveUsageTarget) {
+    ctx.log.error(
+      `Hive usage access token needs a target. Please provide it through the "--hive-usage-target <target>" option or the config.`,
+    );
+    process.exit(1);
+  }
+
+  const hiveUsageAccessToken =
+    opts.hiveUsageAccessToken || opts.hiveRegistryToken;
+  if (hiveUsageAccessToken) {
+    // different logs w and w/o the target to disambiguate
+    if (opts.hiveUsageTarget) {
+      ctx.log.info(`Configuring Hive usage reporting`);
+    } else {
+      ctx.log.info(`Configuring Hive registry reporting`);
     }
-    if (!hiveUsageTarget) {
-      ctx.log.error(
-        `Hive usage access token needs a target. Please provide it through the "--hive-usage-target <target>" option or the config.`,
-      );
-      process.exit(1);
-    }
-    ctx.log.info(`Configuring Hive usage reporting`);
     return {
-      ...loadedConfig.reporting,
       type: 'hive',
-      target: hiveUsageTarget,
       token: hiveUsageAccessToken,
+      target: opts.hiveUsageTarget,
     };
-  } else if (apolloKey) {
+  }
+
+  if (opts.apolloKey) {
     ctx.log.info(`Configuring Apollo GraphOS registry reporting`);
-    if (!apolloGraphRef) {
+    if (!opts.apolloGraphRef) {
       ctx.log.error(
         `Apollo GraphOS requires a graph ref in the format <graph-id>@<graph-variant>. Please provide a valid graph ref.`,
       );
@@ -70,9 +89,10 @@ export function handleReportingConfig(
     }
     return {
       type: 'graphos',
-      apiKey: apolloKey,
-      graphRef: apolloGraphRef,
+      apiKey: opts.apolloKey,
+      graphRef: opts.apolloGraphRef,
     };
   }
+
   return null;
 }
