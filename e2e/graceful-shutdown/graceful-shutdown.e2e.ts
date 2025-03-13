6fb523893a70560e53317189ddb265954cd0ccf8
Denis Badurina <denis@denelop.com>
2025-03-13 20:33:14 +0100

    repro

---
+++ e2e/graceful-shutdown/graceful-shutdown.e2e.ts
@@ -0,0 +1,31 @@
+import { setTimeout } from 'timers/promises';
+import { createExampleSetup, createTenv } from '@internal/e2e';
+import { expect, it } from 'vitest';
+
+const { gateway } = createTenv(__dirname);
+const { supergraph, query, result } = createExampleSetup(__dirname);
+
+it.each(['SIGINT', 'SIGTERM'] as const)(
+  'should gracefully shut down on %s signal',
+  async (signal) => {
+    const gw = await gateway({
+      pipeLogs: 'gw.out',
+      supergraph: await supergraph(),
+    });
+
+    await expect(
+      gw.execute({
+        query,
+      }),
+    ).resolves.toEqual(result);
+
+    gw.kill(signal);
+
+    await Promise.race([
+      gw.waitForExit,
+      setTimeout(3_000).then(() => {
+        expect.fail('Gateway did not exit on signal');
+      }),
+    ]);
+  },
+);
