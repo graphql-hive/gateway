0e2265313b656d4ba88c586f4e15774ece14dd49
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-03-14 16:39:54 +0300

    Go

--- packages/runtime/src/plugins/useWebhooks.ts
+++ packages/runtime/src/plugins/useWebhooks.ts
@@ -20,45 +20,31 @@ export function useWebhooks({
     See documentation: https://the-guild.dev/docs/mesh/pubsub`);
   }
   return {
-    onRequest({ request, url, endResponse, serverContext, fetchAPI }) {
+    onRequest({ request, url, endResponse, fetchAPI }): void | Promise<void> {
       for (const eventName of pubsub.getEventNames()) {
         if (
           eventName ===
           `webhook:${request.method.toLowerCase()}:${url.pathname}`
         ) {
-          logger?.debug(() => `Received webhook request for ${url.pathname}`);
-          function emitEvent() {
-            return request.text().then((body) => {
-              logger?.debug(() => [
-                `Emitted webhook request for ${url.pathname}`,
-                body,
-              ]);
-              pubsub?.publish(
-                eventName,
-                request.headers.get('content-type') === 'application/json'
-                  ? JSON.parse(body)
-                  : body,
-              );
-            });
-          }
-          // Bun handles this differently
-          if (globalThis.Bun) {
-            return emitEvent().finally(() =>
-              endResponse(
-                new fetchAPI.Response(null, {
-                  status: 204,
-                  statusText: 'OK',
-                }),
-              ),
+          logger?.debug(() => [`Received webhook request for ${url.pathname}`]);
+          return request.text().then((body) => {
+            logger?.debug(() => [
+              `Emitted webhook request for ${url.pathname}`,
+              body,
+            ]);
+            pubsub.publish(
+              eventName,
+              request.headers.get('content-type') === 'application/json'
+                ? JSON.parse(body)
+                : body,
             );
-          }
-          serverContext.waitUntil(emitEvent());
-          return endResponse(
-            new fetchAPI.Response(null, {
-              status: 204,
-              statusText: 'OK',
-            }),
-          );
+            endResponse(
+              new fetchAPI.Response(null, {
+                status: 204,
+                statusText: 'OK',
+              }),
+            );
+          });
         }
       }
     },
