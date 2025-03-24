632e22ec1c806ca7d6679203bb84cfdcae3dec17
Denis Badurina <denis@denelop.com>
2025-03-24 15:35:39 +0100

    check gc

--- packages/signal/tests/abortSignalAny.test.ts
+++ packages/signal/tests/abortSignalAny.test.ts
@@ -1,4 +1,6 @@
-import { it } from 'vitest';
+import { setTimeout } from 'timers/promises';
+import LeakDetector from 'jest-leak-detector';
+import { it, vi } from 'vitest';
 import { abortSignalAny } from '../src/abortSignalAny';
 
 it('should not abort if none of the signals abort', ({ expect }) => {
@@ -36,4 +38,53 @@ it('should return aborted signal if aborted before any', ({ expect }) => {
   expect(() => signal.throwIfAborted()).toThrowErrorMatchingInlineSnapshot(
     `"Test"`,
   );
+});
+
+it('should GC all signals after abort', async ({ expect }) => {
+  let ctrl1: AbortController | null = new AbortController();
+  const ctrl1Detector = new LeakDetector(ctrl1);
+  const ctrl1SignalDetector = new LeakDetector(ctrl1.signal);
+  let ctrl2: AbortController | null = new AbortController();
+  const ctrl2Detector = new LeakDetector(ctrl2);
+  const ctrl2SignalDetector = new LeakDetector(ctrl2.signal);
+
+  let signal: AbortSignal | null = abortSignalAny([ctrl1.signal, ctrl2.signal]);
+  const signalDetector = new LeakDetector(signal);
+
+  ctrl1.abort('Test');
+
+  ctrl1 = null;
+  ctrl2 = null;
+  signal = null;
+
+  await expect(ctrl1Detector.isLeaking()).resolves.toBeFalsy();
+  await expect(ctrl1SignalDetector.isLeaking()).resolves.toBeFalsy();
+  await expect(ctrl2Detector.isLeaking()).resolves.toBeFalsy();
+  await expect(ctrl2SignalDetector.isLeaking()).resolves.toBeFalsy();
+  await expect(signalDetector.isLeaking()).resolves.toBeFalsy();
+});
+
+it('should GC all signals without abort', async ({ expect }) => {
+  let ctrl1: AbortController | null = new AbortController();
+  const ctrl1Detector = new LeakDetector(ctrl1);
+  const ctrl1SignalDetector = new LeakDetector(ctrl1.signal);
+  let ctrl2: AbortController | null = new AbortController();
+  const ctrl2Detector = new LeakDetector(ctrl2);
+  const ctrl2SignalDetector = new LeakDetector(ctrl2.signal);
+
+  let signal: AbortSignal | null = abortSignalAny([ctrl1.signal, ctrl2.signal]);
+  const signalDetector = new LeakDetector(signal);
+
+  // no abort
+  // ctrl1.abort('Test');
+
+  ctrl1 = null;
+  ctrl2 = null;
+  signal = null;
+
+  await expect(ctrl1Detector.isLeaking()).resolves.toBeFalsy();
+  await expect(ctrl1SignalDetector.isLeaking()).resolves.toBeFalsy();
+  await expect(ctrl2Detector.isLeaking()).resolves.toBeFalsy();
+  await expect(ctrl2SignalDetector.isLeaking()).resolves.toBeFalsy();
+  await expect(signalDetector.isLeaking()).resolves.toBeFalsy();
 });
