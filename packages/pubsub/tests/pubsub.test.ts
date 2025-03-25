d403afe9734cf14be1b51e41380b573237c8ff22
Denis Badurina <denis@denelop.com>
2025-03-25 16:38:36 +0100

    more tests and dispose

--- packages/pubsub/tests/pubsub.test.ts
+++ packages/pubsub/tests/pubsub.test.ts
@@ -1,4 +1,4 @@
-import { it, vi } from 'vitest';
+import { expect, it, vi } from 'vitest';
 import { PubSub } from '../src/pubsub';
 
 it('should respect topic data generic', () => {
@@ -36,4 +36,53 @@ it('should respect topic data generic', () => {
     'aloha',
     () => {},
   );
+});
+
+it('should publish to relevant subscribers', () => {
+  using pubsub = new PubSub();
+
+  const helloCb = vi.fn();
+  pubsub.subscribe('hello', helloCb);
+
+  const helloCb2 = vi.fn();
+  pubsub.subscribe('hello', helloCb2);
+
+  const alohaCb = vi.fn();
+  pubsub.subscribe('alloha', alohaCb);
+
+  pubsub.publish('hello', 'world');
+
+  expect(helloCb).toHaveBeenCalledTimes(1);
+  expect(helloCb2).toHaveBeenCalledTimes(1);
+  expect(alohaCb).not.toHaveBeenCalled();
+});
+
+it('should not receive topics after unsubscribe', () => {
+  using pubsub = new PubSub();
+
+  const helloCb = vi.fn();
+  const subId = pubsub.subscribe('hello', helloCb);
+
+  pubsub.publish('hello', 'world');
+
+  pubsub.unsubscribe(subId);
+
+  pubsub.publish('hello', 'world');
+
+  expect(helloCb).toHaveBeenCalledTimes(1);
+});
+
+it('should not receive topics after dispose', () => {
+  const pubsub = new PubSub();
+
+  const helloCb = vi.fn();
+  pubsub.subscribe('hello', helloCb);
+
+  pubsub.publish('hello', 'world');
+
+  pubsub.dispose();
+
+  pubsub.publish('hello', 'world');
+
+  expect(helloCb).toHaveBeenCalledTimes(1);
 });
