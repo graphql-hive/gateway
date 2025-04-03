1cc38a92715048b0943b9998b336cd02ede76ae1
Denis Badurina <denis@denelop.com>
2025-04-03 16:46:43 +0200

    unwrap lazy attrs

--- packages/logger/src/utils.ts
+++ packages/logger/src/utils.ts
@@ -11,7 +11,9 @@ export type AttributeValue =
   | undefined
   | (() => AttributeValue); // lazy attribute
 
-export type Attributes = Record<string | number, AttributeValue>;
+export type Attributes =
+  | (() => Attributes)
+  | { [key: string | number]: AttributeValue };
 
 export function logLevelToString(level: LogLevel): string {
   switch (level) {
@@ -52,4 +54,65 @@ export function jsonStringify(val: unknown) {
     }
     return val;
   });
+}
+
+/** Recursivelly unwrapps the lazy attributes. */
+export function unwrapAttrs(attrs: Attributes, depth = 0): Attributes {
+  if (depth > 10) {
+    throw new Error('Too much recursion while unwrapping function attributes');
+  }
+
+  if (typeof attrs === 'function') {
+    return unwrapAttrs(attrs(), depth + 1);
+  }
+
+  const unwrapped: Attributes = {};
+  for (const key of Object.keys(attrs)) {
+    const val = attrs[key as keyof typeof attrs];
+    unwrapped[key] = unwrapAttrVal(val, depth + 1);
+  }
+  return unwrapped;
+}
+
+function unwrapAttrVal(attr: AttributeValue, depth = 0): AttributeValue {
+  if (depth > 10) {
+    throw new Error(
+      'Too much recursion while unwrapping function attribute values',
+    );
+  }
+
+  if (!attr) {
+    return attr;
+  }
+
+  if (isPrimitive(attr)) {
+    return attr;
+  }
+
+  if (typeof attr === 'function') {
+    return unwrapAttrVal(attr(), depth + 1);
+  }
+
+  // unwrap array items
+  if (Array.isArray(attr)) {
+    return attr.map((val) => unwrapAttrVal(val, depth + 1));
+  }
+
+  // plain object (not an instance of anything)
+  // NOTE: is valnurable to `Symbol.toStringTag` pollution, but the user would be sabotaging themselves
+  if (Object.prototype.toString.call(attr) === '[object Object]') {
+    const unwrapped: { [key: string | number]: AttributeValue } = {};
+    for (const key of Object.keys(attr)) {
+      const val = attr[key as keyof typeof attr];
+      unwrapped[key] = unwrapAttrVal(val, depth + 1);
+    }
+    return unwrapped;
+  }
+
+  // very likely an instance of something, dont unwrap it
+  return attr;
+}
+
+function isPrimitive(val: unknown): val is string | number | boolean {
+  return val !== Object(val);
 }
