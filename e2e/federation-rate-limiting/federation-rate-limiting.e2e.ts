3795d45a96475a3ea213c0a66f8703119d4afb61
Denis Badurina <denis@denelop.com>
2025-04-04 16:00:26 +0200

    refine rate limit tests

--- e2e/federation-rate-limiting/federation-rate-limiting.e2e.ts
+++ e2e/federation-rate-limiting/federation-rate-limiting.e2e.ts
@@ -1,13 +1,16 @@
-import { setTimeout } from 'timers/promises';
 import { createExampleSetup, createTenv } from '@internal/e2e';
 import { expect, it } from 'vitest';
 
 const { gateway } = createTenv(__dirname);
 const { supergraph } = createExampleSetup(__dirname);
 
-it('should rate limit requests', async () => {
+it('should rate limit with calmdown', async () => {
+  const rateLimitTtl = 1_000;
   const { execute } = await gateway({
     supergraph: await supergraph(),
+    env: {
+      RATE_LIMIT_TTL: rateLimitTtl,
+    },
   });
 
   const query = /* GraphQL */ `
@@ -18,12 +21,7 @@ it('should rate limit requests', async () => {
     }
   `;
 
-  for (let i = 0; i < 3; i++) {
-    if (i > 0) {
-      // calmdown before testing rate limits again
-      await setTimeout(2_000);
-    }
-
+  for (let i = 0; i < 5; i++) {
     // first 5 requests should not be rate limited
     for (let j = 0; j < 5; j++) {
       await expect(execute({ query })).resolves.toEqual({
@@ -31,18 +29,65 @@ it('should rate limit requests', async () => {
       });
     }
 
-    // subsequent requests are rate limited
+    // then 6th request should be rate limited
+    await expect(execute({ query })).resolves.toEqual({
+      data: {
+        users: null,
+      },
+      errors: expect.arrayContaining([
+        expect.objectContaining({
+          message: 'Rate limit of "Query.users" exceeded for "anonymous"',
+        }),
+      ]),
+    });
+
+    // wait until ttl passes
+    const ttl = AbortSignal.timeout(rateLimitTtl);
+    await new Promise<void>((resolve) => (ttl.onabort = () => resolve()));
+  }
+});
+
+it('should rate limit under pressure', async () => {
+  const rateLimitTtl = 1_000;
+  const { execute } = await gateway({
+    supergraph: await supergraph(),
+    env: {
+      RATE_LIMIT_TTL: rateLimitTtl,
+    },
+  });
+
+  const query = /* GraphQL */ `
+    {
+      users {
+        id
+      }
+    }
+  `;
+
+  for (let i = 0; i < 5; i++) {
+    // first 5 requests should not be rate limited
     for (let j = 0; j < 5; j++) {
       await expect(execute({ query })).resolves.toEqual({
-        data: {
-          users: null,
-        },
-        errors: expect.arrayContaining([
-          expect.objectContaining({
-            message: 'Rate limit of "Query.users" exceeded for "anonymous"',
-          }),
-        ]),
+        data: expect.any(Object),
       });
+    }
+
+    // then 6th request should be rate limited
+    await expect(execute({ query })).resolves.toEqual({
+      data: {
+        users: null,
+      },
+      errors: expect.arrayContaining([
+        expect.objectContaining({
+          message: 'Rate limit of "Query.users" exceeded for "anonymous"',
+        }),
+      ]),
+    });
+
+    // keep spamming until ttl passes
+    const ttl = AbortSignal.timeout(rateLimitTtl);
+    while (!ttl.aborted) {
+      await execute({ query });
     }
   }
 });
--- e2e/federation-rate-limiting/gateway.config.ts
+++ e2e/federation-rate-limiting/gateway.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from '@graphql-hive/gateway';
 
+const rateLimitTtl = parseInt(process.env['RATE_LIMIT_TTL'] || '');
+if (isNaN(rateLimitTtl)) {
+  throw new Error('RATE_LIMIT_TTL must be a number');
+}
+
 export const gatewayConfig = defineConfig({
   rateLimiting: [
     {
       type: 'Query',
       field: 'users',
       max: 5,
-      ttl: 1_000,
+      ttl: rateLimitTtl,
       identifier: 'anonymous',
     },
   ],
