f43016fc6083cd50d2879c90c866d97ff94f71f0
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-04-07 18:31:57 +0300

    ... ENV

--- e2e/federation-mixed/federation-mixed.e2e.ts
+++ e2e/federation-mixed/federation-mixed.e2e.ts
@@ -1,12 +1,14 @@
 import { createExampleSetup, createTenv } from '@internal/e2e';
+import { getLocalhost } from '@internal/testing';
 import { expect, it } from 'vitest';
 
 const { service, gateway } = createTenv(__dirname);
 const exampleSetup = createExampleSetup(__dirname);
 
 it('should execute', async () => {
-  await using inventoryService = await exampleSetup.service('inventory');
-  await using gw = await gateway({
+  const inventoryService = await exampleSetup.service('inventory');
+      const inventoryHost = await getLocalhost(inventoryService.port, inventoryService.protocol);
+  const { execute } = await gateway({
     supergraph: {
       with: 'mesh',
       services: [
@@ -16,12 +18,15 @@ it('should execute', async () => {
         await exampleSetup.service('reviews'),
       ],
       env: {
-        INVENTORY_ENDPOINT: `http://localhost:${inventoryService.port}/graphql`,
+        INVENTORY_ENDPOINT: `${inventoryHost}:${inventoryService.port}/graphql`,
       },
+    },
+    env: {
+      INVENTORY_ENDPOINT: `${inventoryHost}:${inventoryService.port}/graphql`,
     },
   });
   await expect(
-    gw.execute({
+    execute({
       query: exampleSetup.query,
     }),
   ).resolves.toEqual(exampleSetup.result);
