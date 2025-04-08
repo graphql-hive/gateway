367bb33c1d589236b3276cf14bd3331cb3ca1954
Denis Badurina <denis@denelop.com>
2025-04-08 15:57:56 +0200

    fetch with new log

--- packages/runtime/src/plugins/useFetchDebug.ts
+++ packages/runtime/src/plugins/useFetchDebug.ts
@@ -1,38 +1,37 @@
-import type { Logger } from '@graphql-mesh/types';
 import { FetchAPI } from 'graphql-yoga';
 import type { GatewayPlugin } from '../types';
 
-export function useFetchDebug<TContext extends Record<string, any>>(opts: {
-  logger: Logger;
-}): GatewayPlugin<TContext> {
+export function useFetchDebug<
+  TContext extends Record<string, any>,
+>(): GatewayPlugin<TContext> {
   let fetchAPI: FetchAPI;
   return {
     onYogaInit({ yoga }) {
       fetchAPI = yoga.fetchAPI;
     },
-    onFetch({ url, options, logger = opts.logger }) {
+    onFetch({ url, options, context }) {
       const fetchId = fetchAPI.crypto.randomUUID();
-      const fetchLogger = logger.child({
-        fetchId,
-      });
-      const httpFetchRequestLogger = fetchLogger.child('http-fetch-request');
-      httpFetchRequestLogger.debug(() => ({
-        url,
-        ...(options || {}),
-        body: options?.body,
-        headers: options?.headers,
-        signal: options?.signal?.aborted ? options?.signal?.reason : false,
-      }));
+      const log = context.log.child({ fetchId });
+      log.debug(
+        () => ({
+          url,
+          ...(options || {}),
+          body: options?.body,
+          headers: options?.headers,
+          signal: options?.signal?.aborted ? options?.signal?.reason : false,
+        }),
+        'http-fetch-request',
+      );
       const start = performance.now();
       return function onFetchDone({ response }) {
-        const httpFetchResponseLogger = fetchLogger.child(
+        log.debug(
+          () => ({
+            status: response.status,
+            headers: Object.fromEntries(response.headers.entries()),
+            duration: performance.now() - start,
+          }),
           'http-fetch-response',
         );
-        httpFetchResponseLogger.debug(() => ({
-          status: response.status,
-          headers: Object.fromEntries(response.headers.entries()),
-          duration: performance.now() - start,
-        }));
       };
     },
   };
