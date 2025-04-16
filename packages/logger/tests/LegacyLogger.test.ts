6dc001162d44262fb33e7c4cf79e1eb2e8950780
Denis Badurina <denis@denelop.com>
2025-04-16 20:08:19 +0200

    make sure meshlogger fits types of legacylogger

--- packages/logger/tests/LegacyLogger.test.ts
+++ packages/logger/tests/LegacyLogger.test.ts
@@ -1,7 +1,11 @@
 import { LegacyLogger } from '@graphql-hive/logger';
+import { Logger as MeshLogger } from '@graphql-mesh/types';
 import { expect, it } from 'vitest';
 import { Logger, LoggerOptions } from '../src/Logger';
 import { MemoryLogWriter } from '../src/writers';
+
+// a type test making sure the LegacyLogger is compatible with the MeshLogger
+export const _: MeshLogger = new LegacyLogger(null as any);
 
 function createTLogger(opts?: Partial<LoggerOptions>) {
   const writer = new MemoryLogWriter();
