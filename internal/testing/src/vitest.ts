ae686702eed310e929747070a1ed2940f74a9be5
Denis Badurina <denis@denelop.com>
2025-04-17 18:05:48 +0200

    use globalthis

--- internal/testing/src/vitest.ts
+++ internal/testing/src/vitest.ts
@@ -1,7 +1,7 @@
 const cancelledCtrl = new AbortController();
 
 // @ts-expect-error https://github.com/vitest-dev/vitest/issues/7647#issuecomment-2712223721
-__vitest_worker__.onCancel // new line to type-check rest
+globalThis.__vitest_worker__.onCancel // new line to type-check rest
   .then(() => cancelledCtrl.abort('Test run cancelled'));
 
 /** Worker's test run has been cancelled by the user. Using `Q` or a single `CTRL+C`. */
