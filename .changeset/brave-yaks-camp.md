4a80de1a2884dc42a5f09202b6215c3e6780efc4
Mustafa <33766083+MkMan@users.noreply.github.com>
2025-05-28 00:06:26 +1000

    feat(wrap): allow making the new field type Nullable (#1150)
    Co-authored-by: Mustafa Mansour <mmansour@atlassian.com>

---
+++ .changeset/brave-yaks-camp.md
@@ -0,0 +1,5 @@
+---
+'@graphql-tools/wrap': minor
+---
+
+Allow making the new wrapper field type Nullable
--- packages/wrap/tests/transformWrapFields.test.ts
+++ packages/wrap/tests/transformWrapFields.test.ts
@@ -1,6 +1,6 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { WrapFields, wrapSchema } from '@graphql-tools/wrap';
-import { graphql, GraphQLObjectType } from 'graphql';
+import { graphql, GraphQLObjectType, isNullableType } from 'graphql';
 import { describe, expect, test } from 'vitest';
 
 describe('WrapFields', () => {
@@ -44,13 +44,36 @@ describe('WrapFields', () => {
   test('schema is transformed with new type and field', async () => {
     const userType = schema.getType('User') as GraphQLObjectType;
     const addressType = schema.getType('Address') as GraphQLObjectType;
+    const addressField = userType.getFields()['address'];
 
+    expect(isNullableType(addressField?.type)).toBe(false);
     expect(userType.getFields()['address']).toBeDefined();
     expect(Object.keys(addressType.getFields()).sort()).toEqual([
       'city',
       'street',
       'zipcode',
     ]);
+  });
+
+  test('new field is nullable if specified', async () => {
+    const schema = wrapSchema({
+      schema: subschema,
+      transforms: [
+        new WrapFields(
+          'User',
+          ['address'],
+          ['Address'],
+          ['street', 'city', 'zipcode'],
+          undefined,
+          { isNullable: true },
+        ),
+      ],
+    });
+
+    const userType = schema.getType('User') as GraphQLObjectType;
+    const addressField = userType.getFields()['address'];
+
+    expect(isNullableType(addressField?.type)).toBe(true);
   });
 
   test('select fields are wrapped and queryable', async () => {
