dc27c2b81822720bf2a7364fca1feaeff0a9098a
Denis Badurina <denis@denelop.com>
2025-06-09 20:08:58 +0200

    begin

--- packages/federation/tests/global-object-identification.test.ts
+++ packages/federation/tests/global-object-identification.test.ts
@@ -1,9 +1,10 @@
 import { buildSubgraphSchema } from '@apollo/subgraph';
+import { printSchemaWithDirectives } from '@graphql-tools/utils';
 import { parse } from 'graphql';
 import { describe, expect, it } from 'vitest';
-import { stitchLocalSchemas } from './getStitchedSchemaFromLocalSchemas';
+import { getStitchedSchemaFromLocalSchemas } from './getStitchedSchemaFromLocalSchemas';
 
-describe('Relay Object Identification', () => {
+describe('Global Object Identification', () => {
   it('should resolve node by id', async () => {
     const accounts = buildSubgraphSchema({
       typeDefs: parse(/* GraphQL */ `
@@ -27,32 +28,43 @@ describe('Relay Object Identification', () => {
       },
     });
 
-    const { execute } = await stitchLocalSchemas({
+    const schema = await getStitchedSchemaFromLocalSchemas({
       globalObjectIdentification: true,
       localSchemas: {
         accounts,
       },
     });
 
-    await expect(
-      execute({
-        query: /* GraphQL */ `
-          query ($id: ID!) {
-            node(id: $id) {
-              ... on Person {
-                id
-                name
-                email
-              }
-            }
-          }
-        `,
-        variables: { id: '1' },
-      }),
-    ).resolves.toMatchInlineSnapshot(`
-      {
-        "data": {},
+    expect(printSchemaWithDirectives(schema)).toMatchInlineSnapshot(`
+      "schema {
+        query: Query
       }
+
+      type Query {
+        people: [Person!]!
+        """Fetches an object given its globally unique \`ID\`."""
+        node(
+          """The globally unique \`ID\`."""
+          nodeId: ID!
+        ): Node
+      }
+
+      type Person implements Node {
+        id: ID!
+        name: String!
+        email: String!
+        """
+        A globally unique identifier. Can be used in various places throughout the system to identify this single value.
+        """
+        nodeId: ID!
+      }
+
+      interface Node {
+        """
+        A globally unique identifier. Can be used in various places throughout the system to identify this single value.
+        """
+        nodeId: ID!
+      }"
     `);
   });
 });
