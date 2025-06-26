0655d1fc604179b6cc0c148d73e38d6e8d839c56
Denis Badurina <badurinadenis@gmail.com>
2025-06-26 19:08:54 +0200

    Handle multi-error execution results in delegated requests (#1280)

---
+++ .changeset/lemon-cooks-speak.md
@@ -0,0 +1,5 @@
+---
+'@graphql-tools/delegate': patch
+---
+
+Remove trailing spaces when merging multi-error responses
--- packages/delegate/tests/errors.test.ts
+++ packages/delegate/tests/errors.test.ts
@@ -3,6 +3,7 @@ import { stitchSchemas } from '@graphql-tools/stitch';
 import { createGraphQLError, ExecutionResult } from '@graphql-tools/utils';
 import {
   graphql,
+  GraphQLError,
   GraphQLResolveInfo,
   locatedError,
   OperationTypeNode,
@@ -48,62 +49,255 @@ describe('Errors', () => {
       variableValues: {},
     };
 
-    test('persists single error', () => {
-      const result = {
-        errors: [createGraphQLError('Test error')],
-      };
-      try {
-        checkResultAndHandleErrors(result, {
+    test('should return single error', () => {
+      const result = checkResultAndHandleErrors(
+        {
+          errors: [createGraphQLError('Test error')],
+        },
+        {
           fieldName: 'responseKey',
           info: fakeInfo,
-        } as DelegationContext);
-      } catch (e: any) {
-        expect(e.message).toEqual('Test error');
-        expect(e.originalError.errors).toBeUndefined();
-      }
-    });
-
-    test('persists single error with extensions', () => {
-      const result = {
-        errors: [
-          createGraphQLError('Test error', {
-            extensions: {
-              code: 'UNAUTHENTICATED',
-            },
-          }),
-        ],
-      };
-      try {
-        checkResultAndHandleErrors(result, {
-          fieldName: 'responseKey',
-          info: fakeInfo,
-        } as DelegationContext);
-      } catch (e: any) {
-        expect(e.message).toEqual('Test error');
-        expect(e.extensions && e.extensions.code).toEqual('UNAUTHENTICATED');
-        expect(e.originalError.errors).toBeUndefined();
-      }
-    });
-
-    test('combines errors and persists the original errors', () => {
-      const result = {
-        errors: [createGraphQLError('Error1'), createGraphQLError('Error2')],
-      };
-      try {
-        checkResultAndHandleErrors(result, {
-          fieldName: 'responseKey',
-          info: fakeInfo,
-        } as DelegationContext);
-      } catch (e: any) {
-        expect(e.message).toEqual('Error1\nError2');
-        expect(e.originalError).toBeDefined();
-        expect(e.originalError.errors).toBeDefined();
-        expect(e.originalError.errors).toHaveLength(result.errors.length);
-        for (const i in result.errors) {
-          const error = result.errors[i];
-          expect(e.originalError.errors[i]).toEqual(error);
+        } as DelegationContext,
+      );
+      expect(result).toBeInstanceOf(GraphQLError);
+      expect(result.toJSON()).toMatchInlineSnapshot(`
+        {
+          "message": "Test error",
+          "path": [
+            "foo",
+          ],
         }
+      `);
+    });
+
+    test('should return single error with extensions', () => {
+      const result = checkResultAndHandleErrors(
+        {
+          errors: [
+            createGraphQLError('Test error', {
+              extensions: {
+                code: 'UNAUTHENTICATED',
+              },
+            }),
+          ],
+        },
+        {
+          fieldName: 'responseKey',
+          info: fakeInfo,
+        } as DelegationContext,
+      );
+      expect(result).toBeInstanceOf(GraphQLError);
+      expect(result.toJSON()).toMatchInlineSnapshot(`
+        {
+          "extensions": {
+            "code": "UNAUTHENTICATED",
+          },
+          "message": "Test error",
+          "path": [
+            "foo",
+          ],
+        }
+      `);
+    });
+
+    test('should return multiple errors in ExecutionResult format', () => {
+      const result = checkResultAndHandleErrors(
+        {
+          errors: [
+            createGraphQLError('Test error'),
+            createGraphQLError('Test error 2'),
+          ],
+        },
+        {
+          fieldName: 'responseKey',
+          info: fakeInfo,
+        } as DelegationContext,
+      );
+      expect(result.data).toBeUndefined();
+      result.errors.forEach((error: any) => {
+        expect(error).toBeInstanceOf(GraphQLError);
+      });
+      expect(result.errors.map((e: GraphQLError) => e.toJSON()))
+        .toMatchInlineSnapshot(`
+        [
+          {
+            "message": "Test error",
+            "path": [
+              "foo",
+            ],
+          },
+          {
+            "message": "Test error 2",
+            "path": [
+              "foo",
+            ],
+          },
+        ]
+      `);
+    });
+
+    test('should return multiple errors with extensions in ExecutionResult format', () => {
+      const result = checkResultAndHandleErrors(
+        {
+          errors: [
+            createGraphQLError('Test error', {
+              extensions: {
+                code: 'GOOD',
+              },
+            }),
+            createGraphQLError('Test error 2', {
+              extensions: {
+                code: 'VERY_GOOD',
+              },
+            }),
+          ],
+        },
+        {
+          fieldName: 'responseKey',
+          info: fakeInfo,
+        } as DelegationContext,
+      );
+      expect(result.data).toBeUndefined();
+      result.errors.forEach((error: any) => {
+        expect(error).toBeInstanceOf(GraphQLError);
+      });
+      expect(result.errors.map((e: GraphQLError) => e.toJSON()))
+        .toMatchInlineSnapshot(`
+          [
+            {
+              "extensions": {
+                "code": "GOOD",
+              },
+              "message": "Test error",
+              "path": [
+                "foo",
+              ],
+            },
+            {
+              "extensions": {
+                "code": "VERY_GOOD",
+              },
+              "message": "Test error 2",
+              "path": [
+                "foo",
+              ],
+            },
+          ]
+        `);
+    });
+
+    test('should return multiple errors with original errors in ExecutionResult format', () => {
+      const errors = [
+        createGraphQLError('Error1'),
+        createGraphQLError('Error2'),
+      ];
+      const result = checkResultAndHandleErrors(
+        {
+          errors,
+        },
+        {
+          fieldName: 'responseKey',
+          info: fakeInfo,
+        } as DelegationContext,
+      );
+      expect(result.data).toBeUndefined();
+      result.errors.forEach((error: any) => {
+        expect(error).toBeInstanceOf(GraphQLError);
+      });
+      expect(
+        result.errors.map((e: GraphQLError) => ({
+          ...e.toJSON(),
+          originalError: (e.originalError as GraphQLError).toJSON(),
+        })),
+      ).toMatchInlineSnapshot(`
+        [
+          {
+            "message": "Error1",
+            "originalError": {
+              "message": "Error1",
+            },
+            "path": [
+              "foo",
+            ],
+          },
+          {
+            "message": "Error2",
+            "originalError": {
+              "message": "Error2",
+            },
+            "path": [
+              "foo",
+            ],
+          },
+        ]
+      `);
+      for (const i in errors) {
+        const originalError = errors[i];
+        expect(result.errors[i].originalError).toBe(originalError);
       }
+    });
+
+    test('should handle single not-instanceof-Error errors', () => {
+      const result = checkResultAndHandleErrors(
+        {
+          errors: [
+            // @ts-expect-error - testing non-Error error
+            { message: 'Test error' },
+          ],
+        },
+        {
+          fieldName: 'responseKey',
+          info: fakeInfo,
+        } as DelegationContext,
+      );
+      expect(result).toBeInstanceOf(GraphQLError);
+      expect(result.toJSON()).toMatchInlineSnapshot(`
+        {
+          "message": "Test error",
+          "path": [
+            "foo",
+          ],
+        }
+      `);
+    });
+
+    test('should handle multiple not-instanceof-Error errors', () => {
+      // this can happen and has happened to the customer referenced in the pr where this test was added
+      const result = checkResultAndHandleErrors(
+        {
+          errors: [
+            // @ts-expect-error - testing non-Error error
+            { message: 'Test error' },
+            // @ts-expect-error - testing non-Error error
+            { message: 'Test error 2' },
+          ],
+        },
+        {
+          fieldName: 'responseKey',
+          info: fakeInfo,
+        } as DelegationContext,
+      );
+      expect(result.data).toBeUndefined();
+      result.errors.forEach((error: any) => {
+        expect(error).toBeInstanceOf(GraphQLError);
+      });
+      expect(result.errors.map((e: GraphQLError) => e.toJSON()))
+        .toMatchInlineSnapshot(`
+        [
+          {
+            "message": "Test error",
+            "path": [
+              "foo",
+            ],
+          },
+          {
+            "message": "Test error 2",
+            "path": [
+              "foo",
+            ],
+          },
+        ]
+      `);
     });
 
     // see https://github.com/ardatan/graphql-tools/issues/1641
@@ -238,6 +432,157 @@ describe('Errors', () => {
         });
 
         expect(gatewayResult).toEqual(expectedResult);
+      });
+
+      test('should handle multiple errors from multiple fields', async () => {
+        const schema = stitchSchemas({
+          subschemas: [
+            makeExecutableSchema({
+              typeDefs: /* GraphQL */ `
+                type Object {
+                  field1: String
+                  field2: String
+                }
+                type Query {
+                  object: Object
+                }
+              `,
+              resolvers: {
+                Query: {
+                  object: () => ({
+                    field1: () => {
+                      throw new Error('field1 error');
+                    },
+                    field2: () => {
+                      throw new Error('field2 error');
+                    },
+                  }),
+                },
+              },
+            }),
+          ],
+        });
+
+        const result = await graphql({
+          schema,
+          source: /* GraphQL */ `
+            {
+              object {
+                field1
+                field2
+              }
+            }
+          `,
+        });
+
+        expect(result.data).toMatchInlineSnapshot(`
+          {
+            "object": {
+              "field1": null,
+              "field2": null,
+            },
+          }
+        `);
+        result.errors!.forEach((error) => {
+          expect(error).toBeInstanceOf(GraphQLError);
+        });
+        expect(result.errors!.map((e) => e.toJSON())).toMatchInlineSnapshot(`
+          [
+            {
+              "locations": [
+                {
+                  "column": 17,
+                  "line": 4,
+                },
+              ],
+              "message": "field1 error",
+              "path": [
+                "object",
+                "field1",
+              ],
+            },
+            {
+              "locations": [
+                {
+                  "column": 17,
+                  "line": 5,
+                },
+              ],
+              "message": "field2 error",
+              "path": [
+                "object",
+                "field2",
+              ],
+            },
+          ]
+        `);
+      });
+
+      test('should handle multiple errors from single field', async () => {
+        const schema = stitchSchemas({
+          subschemas: [
+            makeExecutableSchema({
+              typeDefs: /* GraphQL */ `
+                type Query {
+                  field: String
+                }
+              `,
+              resolvers: {
+                Query: {
+                  field: () => {
+                    throw new AggregateError([
+                      new Error('field error 1'),
+                      new Error('field error 2'),
+                    ]);
+                  },
+                },
+              },
+            }),
+          ],
+        });
+
+        const result = await graphql({
+          schema,
+          source: /* GraphQL */ `
+            {
+              field
+            }
+          `,
+        });
+
+        expect(result.data).toMatchInlineSnapshot(`
+          {
+            "field": null,
+          }
+        `);
+        result.errors!.forEach((error) => {
+          expect(error).toBeInstanceOf(GraphQLError);
+        });
+        expect(
+          result.errors!.map((e) => {
+            const eobj = e.toJSON();
+            return {
+              ...eobj,
+              // replace all newlines in the message with spaces to pass snapshot for both bun and vitest
+              message: eobj.message.replaceAll('\n', ' '),
+            };
+          }),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "locations": [
+                {
+                  "column": 15,
+                  "line": 3,
+                },
+              ],
+              "message": "field error 1, field error 2",
+              "path": [
+                "field",
+              ],
+            },
+          ]
+        `);
       });
     });
   });
