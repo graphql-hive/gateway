777d90b0ed03f113633015e48474231eda2b8398
Denis Badurina <denis@denelop.com>
2025-06-30 19:13:16 +0200

    disable multipar req

--- packages/runtime/src/createGatewayRuntime.ts
+++ packages/runtime/src/createGatewayRuntime.ts
@@ -1215,6 +1215,7 @@ export function createGatewayRuntime<
     healthCheckEndpoint: config.healthCheckEndpoint || '/healthcheck',
     landingPage: landingPageRenderer,
     disposeOnProcessTerminate: true,
+    multipart: config.multipart ?? false,
   });
 
   fetchAPI ||= yoga.fetchAPI;
