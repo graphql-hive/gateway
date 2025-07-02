f4ed133a10e0294b3125a2f0dd0e05da155be025
Denis Badurina <badurinadenis@gmail.com>
2025-07-02 19:35:51 +0200

    vitest.projects.ts has been deprecated (#1297)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,7 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
+import { timeout as testTimeout } from './internal/e2e/src/timeout';
+import { boolEnv, isCI, isNotPlatform } from './internal/testing/src/env';
 
 // By default, Vite bypasses node_packages to native Node; meaning, imports to
 // packages that match the tsconfig paths wont work because Node will require the
@@ -10,10 +12,66 @@ const inline = [/@graphql-mesh\/.*/, /@omnigraph\/.*/];
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: { server: { deps: { inline } } },
   resolve: {
     alias: {
       graphql: 'graphql/index.js', // TODO: why duplicate graphql errors when there's no multiple graphqls installed? mistery
     },
+  },
+  test: {
+    server: { deps: { inline } },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          include: ['**/*.(test|spec).ts'],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          include: [
+            '**/*.e2e.ts',
+            ...(isCI() && isNotPlatform('linux')
+              ? [
+                  // TODO: containers are not starting on non-linux environments
+                  '!**/e2e/auto-type-merging',
+                  '!**/e2e/neo4j-example',
+                  '!**/e2e/soap-demo',
+                  '!**/e2e/mysql-employees',
+                  '!**/e2e/opentelemetry',
+                  '!**/e2e/graphos-polling',
+                ]
+              : []),
+          ],
+          hookTimeout: testTimeout,
+          testTimeout,
+          retry: boolEnv('CI') ? 3 : 0,
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'bench',
+          hookTimeout: testTimeout,
+          testTimeout,
+          benchmark: {
+            include: ['bench/**/*.bench.ts', 'e2e/**/*.bench.ts'],
+            reporters: ['verbose'],
+            outputJson: 'bench/results.json',
+          },
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'memtest',
+          include: ['**/*.memtest.ts'],
+          hookTimeout: testTimeout,
+          testTimeout,
+        },
+      },
+    ],
   },
 });
