53332d0c461291368f4db2ec9428a14a428be66f
Valentin Cocaud <v.cocaud@gmail.com>
2025-07-10 17:13:30 +0200

    test(gateway-runtime): add e2e test to reproduce #1311
    Revert "test(gateway-runtime): add e2e test to reproduce #1311"
    
    This reverts commit 7caa2f78c4aab428b01601fbd51e1fa8a9a79614.

--- e2e/header-propagation/gateway.config.ts
+++ e2e/header-propagation/gateway.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from '@graphql-hive/gateway';
 
 export const gatewayConfig = defineConfig({
   propagateHeaders: {
-    fromClientToSubgraphs({ request }) {
+    fromClientToSubgraphs({ request, subgraphName }) {
+      if (subgraphName !== 'upstream') {
+        return;
+      }
+
       return {
         authorization: request.headers.get('authorization') ?? 'default',
         'session-cookie-id':
