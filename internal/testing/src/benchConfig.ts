83ba36481793bcaa5fb69270c0780ed7dbad949d
enisdenjo <badurinadenis@gmail.com>
2025-07-15 19:41:42 +0200

    correct bench logic

--- internal/testing/src/benchConfig.ts
+++ internal/testing/src/benchConfig.ts
@@ -1,10 +1,10 @@
 import { getEnvNum, isCI } from '~internal/env';
 import { BenchOptions } from 'vitest';
 
-const duration = (getEnvNum('BENCH_DURATION') ?? isCI()) ? 30000 : 3000;
-const warmupTime = (getEnvNum('BENCH_WARMUP_TIME') ?? isCI()) ? 5000 : 300;
+const duration = getEnvNum('BENCH_DURATION') ?? (isCI() ? 30000 : 3000);
+const warmupTime = getEnvNum('BENCH_WARMUP_TIME') ?? (isCI() ? 5000 : 300);
 const warmupIterations =
-  (getEnvNum('BENCH_WARMUP_ITERATIONS') ?? isCI()) ? 10 : 3;
+  getEnvNum('BENCH_WARMUP_ITERATIONS') ?? (isCI() ? 10 : 3);
 
 export const benchConfig: BenchOptions = {
   time: duration,
