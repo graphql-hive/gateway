2015eeeca1db6cb38289634bd89392b71b443c35
Valentin Cocaud <v.cocaud@gmail.com>
2025-07-25 23:09:09 +0200

    feat(opentelemetry): Add getters for root spans contexts

--- packages/plugins/opentelemetry/tests/yoga.spec.ts
+++ packages/plugins/opentelemetry/tests/yoga.spec.ts
@@ -114,11 +114,11 @@ describe('useOpenTelemetry', () => {
               const createSpan =
                 (name: string) =>
                 (
-                  matcher: Parameters<(typeof otelPlugin)['getOtelContext']>[0],
+                  matcher: Parameters<(typeof otelPlugin)['getActiveContext']>[0],
                 ) =>
                   otelPlugin
                     .getTracer()
-                    .startSpan(name, {}, otelPlugin.getOtelContext(matcher))
+                    .startSpan(name, {}, otelPlugin.getActiveContext(matcher))
                     .end();
 
               return [
