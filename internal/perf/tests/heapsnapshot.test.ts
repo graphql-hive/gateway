0f15cc0bffd340f4b5c3fcebf968b11c773f52e2
enisdenjo <badurinadenis@gmail.com>
2025-08-02 23:43:27 +0200

    use archives for heap snaps

--- internal/perf/tests/heapsnapshot.test.ts
+++ internal/perf/tests/heapsnapshot.test.ts
@@ -1,4 +1,6 @@
+import fs from 'node:fs/promises';
 import path from 'node:path';
+import { spawn } from '@internal/proc';
 import { expect, it } from 'vitest';
 import { leakingObjectsInHeapSnapshotFiles } from '../src/heapsnapshot';
 
@@ -14,25 +16,66 @@ it.skipIf(
     timeout: 30_000,
   },
   async () => {
+    await using snap1 = await archivedFixtureFile(
+      'http-server-under-load/1.heapsnapshot',
+    );
+    await using snap2 = await archivedFixtureFile(
+      'http-server-under-load/2.heapsnapshot',
+    );
+    await using snap3 = await archivedFixtureFile(
+      'http-server-under-load/3.heapsnapshot',
+    );
+    await using snap4 = await archivedFixtureFile(
+      'http-server-under-load/4.heapsnapshot',
+    );
     await expect(
       leakingObjectsInHeapSnapshotFiles([
-        path.join(__fixtures, 'http-server-under-load', '1.heapsnapshot'),
-        path.join(__fixtures, 'http-server-under-load', '2.heapsnapshot'),
-        path.join(__fixtures, 'http-server-under-load', '3.heapsnapshot'),
-        path.join(__fixtures, 'http-server-under-load', '4.heapsnapshot'),
+        snap1.filepath,
+        snap2.filepath,
+        snap3.filepath,
+        snap4.filepath,
       ]),
     ).resolves.toMatchInlineSnapshot(`
-    {
-      "(compiled code)": {
-        "addedCount": 1727,
-        "addedSize": 228096,
-        "countDelta": 91,
-        "name": "(compiled code)",
-        "removedCount": 1636,
-        "removedSize": 163328,
-        "sizeDelta": 64768,
-      },
-    }
-  `);
+      {
+        "(compiled code)": {
+          "addedCount": 1727,
+          "addedSize": 228096,
+          "countDelta": 91,
+          "name": "(compiled code)",
+          "removedCount": 1636,
+          "removedSize": 163328,
+          "sizeDelta": 64768,
+        },
+      }
+    `);
   },
 );
+
+/**
+ * Unarchives the {@link archivedFile provided fixture file} for using in
+ * tests and then removes it on disposal.
+ *
+ * @param archivedFile - The name of the archived fixture file (without `.tar.gz`). Is the
+ * filename of the file inside the archive.
+ */
+async function archivedFixtureFile(archivedFile: string) {
+  const filepath = path.join(__fixtures, archivedFile);
+  const [, waitForExit] = await spawn(
+    {
+      cwd: __fixtures,
+    },
+    'tar',
+    '-xz',
+    '-f',
+    archivedFile + '.tar.gz',
+    '-C',
+    path.dirname(filepath),
+  );
+  await waitForExit;
+  return {
+    [Symbol.asyncDispose]() {
+      return fs.unlink(filepath);
+    },
+    filepath,
+  };
+}
