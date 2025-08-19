61c11115639ca1a1403012e4ae54fe32325306e8
Denis Badurina <denis@denelop.com>
2025-08-19 17:56:39 +0200

    new pubsub

--- packages/pubsub/src/mem.ts
+++ packages/pubsub/src/mem.ts
@@ -1,31 +1,10 @@
 import { Repeater } from '@repeaterjs/repeater';
 import { DisposableSymbols } from '@whatwg-node/disposablestack';
 import type { MaybePromise } from '@whatwg-node/promise-helpers';
-import type { Redis } from 'ioredis';
 import { PubSub, PubSubListener, TopicDataMap } from './pubsub';
 
-/**
- * When a Redis connection enters "subscriber mode" (after calling SUBSCRIBE), it can only execute
- * subscriber commands (SUBSCRIBE, UNSUBSCRIBE, etc.). Meaning, it cannot execute other commands like PUBLISH.
- * To avoid this, we use two separate Redis clients: one for publishing and one for subscribing.
- */
-export interface RedisPubSubConnections {
-  /** The redis instance that publishes events/topics. */
-  pub: Redis;
-  /** The redis instance that listens and subscribes to events/topics. */
-  sub: Redis;
-}
-
-export interface RedisPubSubOptions {
-  /**
-   * Prefix for Redis channels to avoid conflicts
-   * @default '@graphql-hive/pubsub:'
-   */
-  channelPrefix?: string;
-}
-
 /** In-memory {@link PubSub} implementation. */
-export class RedisPubSub<M extends TopicDataMap = TopicDataMap>
+export class MemPubSub<M extends TopicDataMap = TopicDataMap>
   implements PubSub<M>
 {
   #disposed = false;
@@ -37,25 +16,6 @@ export class RedisPubSub<M extends TopicDataMap = TopicDataMap>
     >
   >();
 
-  #redis: RedisPubSubConnections;
-  #channelPrefix: string;
-
-  #boundHandleMessage: (channel: string, message: string) => void;
-
-  constructor(redis: RedisPubSubConnections, options: RedisPubSubOptions = {}) {
-    this.#redis = redis;
-    this.#channelPrefix = options.channelPrefix || '@graphql-hive/pubsub:';
-    this.#boundHandleMessage = this.#handleMessage.bind(this);
-    this.#redis.sub.on('message', this.#boundHandleMessage);
-  }
-
-  #topicToChannel(topic: keyof M): string {
-    return `${this.#channelPrefix}${String(topic)}`;
-  }
-  #topicFromChannel(channel: string): string {
-    return channel.replace(this.#channelPrefix, '');
-  }
-
   public subscribedTopics() {
     return this.#subscribers.keys();
   }
@@ -64,21 +24,6 @@ export class RedisPubSub<M extends TopicDataMap = TopicDataMap>
     if (this.#disposed) {
       throw new Error('PubSub is disposed, cannot publish data');
     }
-    return new Promise<void>((resolve, reject) => {
-      this.#redis.pub.publish(
-        this.#topicToChannel(topic),
-        JSON.stringify(data),
-        (e) => {
-          if (e) reject(e);
-          else resolve();
-        },
-      );
-    });
-  }
-
-  #handleMessage(channel: string, message: string) {
-    const topic = this.#topicFromChannel(channel);
-    const data = JSON.parse(message);
     const listeners = this.#subscribers.get(topic);
     if (listeners) {
       for (const l of listeners.keys()) {
@@ -109,43 +54,30 @@ export class RedisPubSub<M extends TopicDataMap = TopicDataMap>
 
     if (!listener) {
       return new Repeater<M[Topic], any, any>(async (push, stop) => {
-        await this.#redis.sub.subscribe(this.#topicToChannel(topic));
         listeners.set(push, stop);
         await stop;
         listeners.delete(push);
         if (listeners.size === 0) {
           this.#subscribers.delete(topic);
         }
-        await this.#redis.sub.unsubscribe(this.#topicToChannel(topic));
       });
     }
 
     const listenerRef = new WeakRef(listener);
-    const unsubscribe = async () => {
+    const unsubscribe = () => {
       const l = listenerRef.deref(); // dont hold on to the listener
       if (l) listeners!.delete(l);
       if (listeners!.size === 0) {
         this.#subscribers.delete(topic);
       }
-      await this.#redis.sub.unsubscribe(this.#topicToChannel(topic));
     };
     listeners.set(listener, unsubscribe);
 
-    return new Promise((resolve, reject) => {
-      this.#redis.sub.subscribe(this.#topicToChannel(topic), (err) => {
-        if (err) {
-          unsubscribe();
-          reject(err);
-        } else {
-          resolve(unsubscribe);
-        }
-      });
-    });
+    return unsubscribe;
   }
 
   public async dispose() {
     this.#disposed = true;
-    this.#redis.sub.off('message', this.#boundHandleMessage);
     for (const stop of this.#subscribers.values().flatMap((s) => s.values())) {
       stop();
     }
--- packages/pubsub/tests/pubsub.test.ts
+++ packages/pubsub/tests/pubsub.test.ts
@@ -1,128 +1,180 @@
+import { Container, createTenv } from '@internal/e2e';
+import Redis from 'ioredis';
 import LeakDetector from 'jest-leak-detector';
-import { expect, it, vi } from 'vitest';
-import { PubSub } from '../src/pubsub';
+import { describe, expect, it, vi } from 'vitest';
+import { MemPubSub } from '../src/mem';
+import { PubSub as IPubSub, TopicDataMap } from '../src/pubsub';
+import { RedisPubSub } from '../src/redis';
 
-it('should respect topic data generic', () => {
-  using pubsub = new PubSub<{ hello: 'world'; obj: Record<string, any> }>();
+const PubSubCtors = [MemPubSub, RedisPubSub];
 
-  pubsub.publish('hello', 'world');
-
-  pubsub.publish(
-    'hello',
-    // @ts-expect-error must be 'world'
-    0,
-  );
-
-  pubsub.publish(
-    // @ts-expect-error does not exist in map
-    'aloha',
-    0,
-  );
-
-  pubsub.publish('obj', {});
-
-  pubsub.publish(
-    'obj',
-    // @ts-expect-error must be an object
-    '{}',
-  );
-
-  pubsub.subscribe('hello', (data) => {
-    // @ts-expect-error must be 'world'
-    data();
-  });
-
-  pubsub.subscribe(
-    // @ts-expect-error does not exist in map
-    'aloha',
-    () => {},
-  );
-});
-
-it('should publish to relevant subscribers', () => {
-  using pubsub = new PubSub();
-
-  const helloCb = vi.fn();
-  pubsub.subscribe('hello', helloCb);
-
-  const helloCb2 = vi.fn();
-  pubsub.subscribe('hello', helloCb2);
-
-  const alohaCb = vi.fn();
-  pubsub.subscribe('alloha', alohaCb);
-
-  pubsub.publish('hello', 'world');
-
-  expect(helloCb).toHaveBeenCalledTimes(1);
-  expect(helloCb2).toHaveBeenCalledTimes(1);
-  expect(alohaCb).not.toHaveBeenCalled();
-});
-
-it('should not receive topics after unsubscribe', () => {
-  using pubsub = new PubSub();
-
-  const helloCb = vi.fn();
-  const subId = pubsub.subscribe('hello', helloCb);
-
-  pubsub.publish('hello', 'world');
-
-  pubsub.unsubscribe(subId);
-
-  pubsub.publish('hello', 'world');
-
-  expect(helloCb).toHaveBeenCalledTimes(1);
-});
-
-it('should not receive topics after dispose', () => {
-  const pubsub = new PubSub();
-
-  const helloCb = vi.fn();
-  pubsub.subscribe('hello', helloCb);
-
-  pubsub.publish('hello', 'world');
-
-  pubsub.dispose();
-
-  pubsub.publish('hello', 'world');
-
-  expect(helloCb).toHaveBeenCalledTimes(1);
-});
-
-it('should return async iterables after dispose', async () => {
-  const pubsub = new PubSub();
-
-  const iter = (async () => {
-    for await (const _data of pubsub.asyncIterator('hello')) {
+for (const PubSub of PubSubCtors) {
+  describe(PubSub.name, async () => {
+    let redis: Container | null = null;
+    if (PubSub === RedisPubSub) {
+      const { container } = createTenv(__dirname);
+      redis = await container({
+        name: 'redis',
+        image: 'redis:8',
+        containerPort: 6379,
+        healthcheck: ['CMD-SHELL', 'redis-cli ping'],
+        env: {
+          LANG: '', // fixes "Failed to configure LOCALE for invalid locale name."
+        },
+      });
     }
-    // wont break if not disposed
-  })();
 
-  pubsub.dispose();
+    async function createPubSub<Data extends TopicDataMap>(): Promise<
+      IPubSub<Data>
+    > {
+      if (PubSub === MemPubSub) {
+        return new PubSub<Data>();
+      }
+      if (PubSub === RedisPubSub) {
+        if (!redis) {
+          throw new Error('Redis container is not initialized');
+        }
+        const pub = new Redis({ port: redis.port });
+        const sub = new Redis({ port: redis.port });
+        await new Promise((resolve, reject) => {
+          pub.once('connect', resolve);
+          pub.once('error', reject);
+        });
+        await new Promise((resolve, reject) => {
+          sub.once('connect', resolve);
+          pub.once('error', reject);
+        });
+        return new RedisPubSub<Data>({ pub, sub });
+      }
+      throw new Error(`Unsupported PubSub implementation: ${PubSub.name}`);
+    }
 
-  await expect(iter).resolves.toBeUndefined();
-});
+    it('should respect topic data generic', async () => {
+      await using pubsub = await createPubSub<{
+        hello: 'world';
+        obj: Record<string, any>;
+      }>();
 
-it.skipIf(
-  // leak detector doesnt work with bun because setFlagsFromString is not yet implemented in Bun
-  // we also assume that bun doesnt leak
-  globalThis.Bun,
-)('should GC listener after unsubscribe', async () => {
-  const pubsub = new PubSub();
+      pubsub.publish('hello', 'world');
 
-  let cb: null | (() => void) = () => {
-    // noop
-  };
-  const cbDetector = new LeakDetector(cb);
+      pubsub.publish(
+        'hello',
+        // @ts-expect-error must be 'world'
+        0,
+      );
 
-  const subId = pubsub.subscribe('hello', cb);
+      pubsub.publish(
+        // @ts-expect-error does not exist in map
+        'aloha',
+        0,
+      );
 
-  pubsub.publish('hello', 'world');
+      pubsub.publish('obj', {});
 
-  cb = null;
+      pubsub.publish(
+        'obj',
+        // @ts-expect-error must be an object
+        '{}',
+      );
 
-  await expect(cbDetector.isLeaking()).resolves.toBeTruthy(); // since not yet unsubscribed
+      pubsub.subscribe('hello', (data) => {
+        // @ts-expect-error must be 'world'
+        data();
+      });
 
-  pubsub.unsubscribe(subId);
+      pubsub.subscribe(
+        // @ts-expect-error does not exist in map
+        'aloha',
+        () => {},
+      );
+    });
 
-  await expect(cbDetector.isLeaking()).resolves.toBeFalsy(); // unsubscribed
-});
+    it('should publish to relevant subscribers', async () => {
+      await using pubsub = await createPubSub();
+
+      const helloCb = vi.fn();
+      await pubsub.subscribe('hello', helloCb);
+
+      const helloCb2 = vi.fn();
+      await pubsub.subscribe('hello', helloCb2);
+
+      const alohaCb = vi.fn();
+      await pubsub.subscribe('alloha', alohaCb);
+
+      await pubsub.publish('hello', 'world');
+
+      expect(helloCb).toHaveBeenCalledTimes(1);
+      expect(helloCb2).toHaveBeenCalledTimes(1);
+      expect(alohaCb).not.toHaveBeenCalled();
+    });
+
+    it('should not receive topics after unsubscribe', async () => {
+      await using pubsub = await createPubSub();
+
+      const helloCb = vi.fn();
+      const unsub = await pubsub.subscribe('hello', helloCb);
+
+      await pubsub.publish('hello', 'world');
+
+      await unsub();
+
+      await pubsub.publish('hello', 'world');
+
+      expect(helloCb).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not receive topics after dispose', async () => {
+      const pubsub = await createPubSub();
+
+      const helloCb = vi.fn();
+      await pubsub.subscribe('hello', helloCb);
+
+      await pubsub.publish('hello', 'world');
+
+      await pubsub.dispose();
+
+      expect(() => pubsub.publish('hello', 'world')).toThrow();
+
+      expect(helloCb).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return async iterables after dispose', async () => {
+      const pubsub = await createPubSub();
+
+      const iter = (async () => {
+        for await (const _data of pubsub.subscribe('hello')) {
+        }
+        // wont break if not disposed
+      })();
+
+      await pubsub.dispose();
+
+      await expect(iter).resolves.toBeUndefined();
+    });
+
+    it.skipIf(
+      // leak detector doesnt work with bun because setFlagsFromString is not yet implemented in Bun
+      // we also assume that bun doesnt leak
+      globalThis.Bun,
+    )('should GC listener after unsubscribe', async () => {
+      const pubsub = await createPubSub();
+
+      let cb: null | (() => void) = () => {
+        // noop
+      };
+      const cbDetector = new LeakDetector(cb);
+
+      const unsub = await pubsub.subscribe('hello', cb);
+
+      await pubsub.publish('hello', 'world');
+
+      cb = null;
+
+      await expect(cbDetector.isLeaking()).resolves.toBeTruthy(); // since not yet unsubscribed
+
+      await unsub();
+
+      await expect(cbDetector.isLeaking()).resolves.toBeFalsy(); // unsubscribed
+    });
+  });
+}
