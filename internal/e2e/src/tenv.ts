787617a2d4b03ff3a873ecef2765ed6d1eb96b6b
Denis Badurina <denis@denelop.com>
2025-08-21 17:54:34 +0200

    cancel signal is not available in jest

--- internal/e2e/src/tenv.ts
+++ internal/e2e/src/tenv.ts
@@ -820,7 +820,11 @@ export function createTenv(cwd: string): Tenv {
         .catch(() => false);
 
       const ctrl = new AbortController();
-      const signal = AbortSignal.any([ctrl.signal, cancelledSignal]);
+      const signal = AbortSignal.any(
+        [ctrl.signal, cancelledSignal]
+          // we filter because cancelledSignal is not present in jest
+          .filter(Boolean),
+      );
 
       if (!bakedImage) {
         // pull image if it doesnt exist and wait for finish
