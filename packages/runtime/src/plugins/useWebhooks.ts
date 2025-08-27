09b2db0dca94bad8faf3818eacfce9456a20ba47
Denis Badurina <denis@denelop.com>
2025-08-27 17:55:36 +0200

    not always a promise

--- packages/runtime/src/plugins/useWebhooks.ts
+++ packages/runtime/src/plugins/useWebhooks.ts
@@ -24,41 +24,45 @@ export function useWebhooks({
     See documentation: https://the-guild.dev/docs/mesh/pubsub`);
   }
   return {
-    async onRequest({ request, url, endResponse, fetchAPI }) {
-      const topics = await pubsub.subscribedTopics();
-      if (Array(topics).length === 0) return;
-      const requestMethod = request.method.toLowerCase();
-      const pathname = url.pathname;
-      const expectedEventName = `webhook:${requestMethod}:${pathname}`;
-      for (const eventName of topics) {
-        if (eventName === expectedEventName) {
-          log.debug({ pathname }, 'Received webhook request');
-          return handleMaybePromise(
-            () => request.text(),
-            function handleWebhookPayload(webhookPayload) {
-              log.debug(
-                { pathname, payload: webhookPayload },
-                'Emitted webhook request',
-              );
-              webhookPayload =
-                request.headers.get('content-type') === 'application/json'
-                  ? JSON.parse(webhookPayload)
-                  : webhookPayload;
+    onRequest({ request, url, endResponse, fetchAPI }) {
+      return handleMaybePromise(
+        () => pubsub.subscribedTopics(),
+        (topics) => {
+          if (Array(topics).length === 0) return;
+          const requestMethod = request.method.toLowerCase();
+          const pathname = url.pathname;
+          const expectedEventName = `webhook:${requestMethod}:${pathname}`;
+          for (const eventName of topics) {
+            if (eventName === expectedEventName) {
+              log.debug({ pathname }, 'Received webhook request');
               return handleMaybePromise(
-                () => pubsub.publish(eventName, webhookPayload),
-                () => {
-                  endResponse(
-                    new fetchAPI.Response(null, {
-                      status: 204,
-                      statusText: 'OK',
-                    }),
+                () => request.text(),
+                function handleWebhookPayload(webhookPayload) {
+                  log.debug(
+                    { pathname, payload: webhookPayload },
+                    'Emitted webhook request',
+                  );
+                  webhookPayload =
+                    request.headers.get('content-type') === 'application/json'
+                      ? JSON.parse(webhookPayload)
+                      : webhookPayload;
+                  return handleMaybePromise(
+                    () => pubsub.publish(eventName, webhookPayload),
+                    () => {
+                      endResponse(
+                        new fetchAPI.Response(null, {
+                          status: 204,
+                          statusText: 'OK',
+                        }),
+                      );
+                    },
                   );
                 },
               );
-            },
-          );
-        }
-      }
+            }
+          }
+        },
+      );
     },
   };
 }
