d457cecd3862f55cda3725b36414e24d18ebbe41
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-15 18:50:27 +0300

    F

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ import JSON5 from 'json5';
 import { pathsToModuleNameMapper } from 'ts-jest';
 import { isCI } from './internal/env/src/index';
 
-const rootDir = __dirname;
+const rootDir = process.cwd();
 const tsconfigPath = resolve(rootDir, 'tsconfig.json');
 const tsconfigContents = readFileSync(tsconfigPath, 'utf8');
 const tsconfig = JSON5.parse(tsconfigContents);
