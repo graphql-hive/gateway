bb9905fa22f9cde363771a3d706f7c31a828a1ed
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-24 17:35:58 +0300

    Format

--- packages/batch-delegate/tests/errorPaths.test.ts
+++ packages/batch-delegate/tests/errorPaths.test.ts
@@ -1,30 +1,30 @@
-import { GraphQLError, parse } from 'graphql';
 import { batchDelegateToSchema } from '@graphql-tools/batch-delegate';
 import { delegateToSchema } from '@graphql-tools/delegate';
+import { normalizedExecutor } from '@graphql-tools/executor';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { stitchSchemas } from '@graphql-tools/stitch';
+import { GraphQLError, parse } from 'graphql';
 import { beforeEach, describe, expect, test, vi } from 'vitest';
-import { normalizedExecutor } from '@graphql-tools/executor';
 
 class NotFoundError extends GraphQLError {
-    constructor(id: unknown) {
-        super('Not Found', {
-            extensions: { id },
-        })
-    }
+  constructor(id: unknown) {
+    super('Not Found', {
+      extensions: { id },
+    });
+  }
 }
 
 describe('preserves error path indices', () => {
-    const getProperty = vi.fn((id: unknown) => {
-        return new NotFoundError(id);
-    });
+  const getProperty = vi.fn((id: unknown) => {
+    return new NotFoundError(id);
+  });
 
-    beforeEach(() => {
-        getProperty.mockClear();
-    });
+  beforeEach(() => {
+    getProperty.mockClear();
+  });
 
-    const subschema = makeExecutableSchema({
-        typeDefs: /* GraphQL */ `
+  const subschema = makeExecutableSchema({
+    typeDefs: /* GraphQL */ `
       type Property {
         id: ID!
       }
@@ -40,28 +40,28 @@ describe('preserves error path indices', () => {
         propertiesByIds(ids: [ID!]!): [Property]!
       }
     `,
-        resolvers: {
-            Query: {
-                objects: () => {
-                    return [
-                        { id: '1', propertyId: '1' },
-                        { id: '2', propertyId: '1' },
-                    ];
-                },
-                propertyById: (_, args) => getProperty(args.id),
-                propertiesByIds: (_, args) => args.ids.map(getProperty),
-            },
+    resolvers: {
+      Query: {
+        objects: () => {
+          return [
+            { id: '1', propertyId: '1' },
+            { id: '2', propertyId: '1' },
+          ];
         },
-    });
+        propertyById: (_, args) => getProperty(args.id),
+        propertiesByIds: (_, args) => args.ids.map(getProperty),
+      },
+    },
+  });
 
-    const subschemas = [subschema];
-    const typeDefs = /* GraphQL */ `
+  const subschemas = [subschema];
+  const typeDefs = /* GraphQL */ `
     extend type Object {
       property: Property
     }
   `;
 
-    const query = /* GraphQL */ `
+  const query = /* GraphQL */ `
     query {
       objects {
         id
@@ -72,91 +72,91 @@ describe('preserves error path indices', () => {
     }
   `;
 
-    const expected = {
-        errors: [
-            {
-                message: 'Not Found',
-                extensions: { id: '1' },
-                path: ['objects', 0, 'property'],
-            },
-            {
-                message: 'Not Found',
-                extensions: { id: '1' },
-                path: ['objects', 1, 'property'],
-            },
-        ],
-        data: {
-            objects: [
-                {
-                    id: '1',
-                    property: null,
-                },
-                {
-                    id: '2',
-                    property: null,
-                },
-            ],
+  const expected = {
+    errors: [
+      {
+        message: 'Not Found',
+        extensions: { id: '1' },
+        path: ['objects', 0, 'property'],
+      },
+      {
+        message: 'Not Found',
+        extensions: { id: '1' },
+        path: ['objects', 1, 'property'],
+      },
+    ],
+    data: {
+      objects: [
+        {
+          id: '1',
+          property: null as null,
         },
-    };
+        {
+          id: '2',
+          property: null as null,
+        },
+      ],
+    },
+  };
 
-    test('using delegateToSchema', async () => {
-        const schema = stitchSchemas({
-            subschemas,
-            typeDefs,
-            resolvers: {
-                Object: {
-                    property: {
-                        selectionSet: '{ propertyId }',
-                        resolve: (source, _, context, info) => {
-                            return delegateToSchema({
-                                schema: subschema,
-                                fieldName: 'propertyById',
-                                args: { id: source.propertyId },
-                                context,
-                                info,
-                            });
-                        },
-                    },
-                },
+  test('using delegateToSchema', async () => {
+    const schema = stitchSchemas({
+      subschemas,
+      typeDefs,
+      resolvers: {
+        Object: {
+          property: {
+            selectionSet: '{ propertyId }',
+            resolve: (source, _, context, info) => {
+              return delegateToSchema({
+                schema: subschema,
+                fieldName: 'propertyById',
+                args: { id: source.propertyId },
+                context,
+                info,
+              });
             },
-        });
-
-
-        const result = await normalizedExecutor({
-            schema,
-            document: parse(query),
-        })
-
-        expect(getProperty).toBeCalledTimes(2);
-        expect(result).toMatchObject(expected);
+          },
+        },
+      },
     });
 
-    test('using batchDelegateToSchema', async () => {
-        const schema = stitchSchemas({
-            subschemas,
-            typeDefs,
-            resolvers: {
-                Object: {
-                    property: {
-                        selectionSet: '{ propertyId }',
-                        resolve: (source, _, context, info) => batchDelegateToSchema({
-                            schema: subschema,
-                            fieldName: 'propertiesByIds',
-                            key: source.propertyId,
-                            context,
-                            info,
-                        }),
-                    },
-                },
-            },
-        });
-
-        const result = await normalizedExecutor({
-            schema,
-            document: parse(query),
-        })
-
-        expect(getProperty).toBeCalledTimes(1);
-        expect(result).toMatchObject(expected);
+    const result = await normalizedExecutor({
+      schema,
+      document: parse(query),
     });
+
+    expect(getProperty).toBeCalledTimes(2);
+    expect(result).toMatchObject(expected);
+  });
+
+  test('using batchDelegateToSchema', async () => {
+    const schema = stitchSchemas({
+      subschemas,
+      typeDefs,
+      resolvers: {
+        Object: {
+          property: {
+            selectionSet: '{ propertyId }',
+            resolve: (source, _, context, info) =>
+              batchDelegateToSchema({
+                schema: subschema,
+                fieldName: 'propertiesByIds',
+                key: source.propertyId,
+                context,
+                info,
+              }),
+          },
+        },
+      },
+    });
+
+    const result = await normalizedExecutor({
+      schema,
+      document: parse(query),
+    });
+
+    expect(getProperty).toBeCalledTimes(1);
+    expect(result).toMatchObject(expected);
+  });
 });
