fcd5cd9581a4e0c7b5608735b0bddd5621d70c53
Denis Badurina <denis@denelop.com>
2025-10-08 17:27:50 +0200

    memtest logger

---
+++ packages/logger/tests/logger.memtest.ts
@@ -0,0 +1,48 @@
+import path from 'path';
+import { memtest } from '@internal/perf/memtest';
+import { spawn, waitForPort } from '@internal/proc';
+import { getAvailablePort } from '@internal/testing';
+import { describe } from 'vitest';
+
+const cwd = __dirname;
+
+describe.concurrent.each([
+  'sync-logging',
+  'async-logging',
+  'child-loggers',
+  'large-attributes',
+  'circular-refs',
+  'lazy-attributes',
+  'mixed-levels',
+  'level-changes',
+])('logger memtest for %s', (name) => {
+  memtest(
+    {
+      cwd,
+      pathname: `/${name}`,
+    },
+    async () => {
+      const port = await getAvailablePort();
+      const [proc] = await spawn(
+        { cwd, env: { PORT: port } },
+        'node',
+        '--inspect-port=0', // necessary for perf inspector
+        '--import',
+        'tsx',
+        path.join(cwd, 'logger-memtest-server.ts'),
+      );
+
+      await waitForPort({
+        port,
+        protocol: 'http',
+        signal: new AbortController().signal,
+      });
+
+      return {
+        ...proc,
+        port,
+        protocol: 'http',
+      };
+    },
+  );
+});
