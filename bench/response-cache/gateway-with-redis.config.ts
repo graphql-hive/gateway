2e322daa3a7c4fd9aa6089229a1390a1b6900f97
Valentin Cocaud <v.cocaud@gmail.com>
2025-10-13 10:58:19 +0200

    :construction:

--- bench/response-cache/gateway-with-redis.config.ts
+++ bench/response-cache/gateway-with-redis.config.ts
@@ -1,17 +1,38 @@
 import { defineConfig } from '@graphql-hive/gateway';
+import { openTelemetrySetup } from '@graphql-hive/gateway/opentelemetry/setup';
+import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
+const RESPONSE_CACHE_ENABLED = process.env['RESPONSE_CACHE_ENABLED'] == 'true';
+
+openTelemetrySetup({
+  contextManager: new AsyncLocalStorageContextManager(),
+  resource: {
+    serviceName: RESPONSE_CACHE_ENABLED ? 'with-cache' : 'without-cache',
+    serviceVersion: '1.0.0',
+  },
+  traces: {
+    exporter: new OTLPTraceExporter({ url: 'http://localhost:4318/v1/traces' }),
+    // batching: false,
+  },
+});
 export const gatewayConfig = defineConfig({
+  openTelemetry: {
+    traces: true,
+  },
   cache: {
     type: 'redis',
     url: process.env['REDIS_URL'], // The URL of the Redis server
     lazyConnect: false,
   },
-  responseCaching: {
-    ttl: 0,
-    ttlPerType: {
-      'Query.me': 2000,
-    },
-    session: () => null,
-  },
+  responseCaching: RESPONSE_CACHE_ENABLED
+    ? {
+        ttl: 0,
+        ttlPerType: {
+          'Query.me': 2000,
+        },
+        session: () => null,
+      }
+    : undefined,
   maskedErrors: false,
 });
--- bench/response-cache/response-cache.bench.ts
+++ bench/response-cache/response-cache.bench.ts
@@ -1,4 +1,3 @@
-import { setTimeout } from 'node:timers/promises';
 import { createExampleSetup, createTenv, GatewayOptions } from '@internal/e2e';
 import { benchConfig } from '@internal/testing';
 import { bench, describe, expect } from 'vitest';
@@ -16,22 +15,33 @@ describe('Response Cache', async () => {
     containerPort: 6379,
   });
 
-  await runBench(
+  await runBench.skip(
     'With in memory response cache',
     'gateway-with-cache.config.ts',
   );
-  await runBench('Without response cache', 'gateway-without-cache.config.ts');
+  await runBench.skip(
+    'Without response cache',
+    'gateway-without-cache.config.ts',
+  );
 
   await runBench.skip(
     'Without invalidation cache',
     'gateway-without-auto-invalidation.config.ts',
   );
 
-  await runBench.skip(
-    'With redis response cache',
+  await runBench('With redis response cache', 'gateway-with-redis.config.ts', {
+    env: {
+      RESPONSE_CACHE_ENABLED: 'true',
+      REDIS_URL: `redis://localhost:${redis.port}`,
+    },
+  });
+
+  await runBench(
+    'With redis but no response cache',
     'gateway-with-redis.config.ts',
     {
       env: {
+        RESPONSE_CACHE_ENABLED: 'false',
         REDIS_URL: `redis://localhost:${redis.port}`,
       },
     },
