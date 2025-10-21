29cff9018cc492f1ceefcfcd3b10a17e4c705e91
Denis Badurina <denis@denelop.com>
2025-10-21 15:04:10 +0200

    subgraphs get

--- packages/fusion-runtime/src/router/handler.ts
+++ packages/fusion-runtime/src/router/handler.ts
@@ -1,16 +1,21 @@
 import { QueryPlanner } from '@graphql-hive/router';
-import type {
-  UnifiedGraphHandlerOpts,
-  UnifiedGraphHandlerResult,
+import {
+  type UnifiedGraphHandlerOpts,
+  type UnifiedGraphHandlerResult,
 } from '@graphql-mesh/fusion-runtime';
 import { memoize1, printSchemaWithDirectives } from '@graphql-tools/utils';
-import { DocumentNode, GraphQLSchema, print } from 'graphql';
+import { DocumentNode, print } from 'graphql';
+import { handleFederationSupergraph } from '../federation/supergraph';
 import { executeQueryPlan } from './executor';
 
 export function handleFederationSupergraphWithRouter(
   opts: UnifiedGraphHandlerOpts,
 ): UnifiedGraphHandlerResult {
+  // TODO: should we do it this way? we only need the tools handler to pluck out the subgraphs
+  const { getSubgraphSchema } = handleFederationSupergraph(opts);
+
   const unifiedGraph = opts.unifiedGraph;
+
   const qp = new QueryPlanner(printSchemaWithDirectives(unifiedGraph));
   const plan = memoize1(function plan(document: DocumentNode) {
     const queryPlan = qp.plan(print(document));
@@ -18,13 +23,7 @@ export function handleFederationSupergraphWithRouter(
   });
   return {
     unifiedGraph,
-    getSubgraphSchema(subgraphName) {
-      const subgraphSchema: GraphQLSchema | undefined = undefined;
-      if (!subgraphSchema) {
-        throw new Error(`Subgraph ${subgraphName} not found`);
-      }
-      return subgraphSchema;
-    },
+    getSubgraphSchema,
     executor({ document, variables, operationName, context }) {
       return plan(document).then((queryPlan) =>
         executeQueryPlan({
