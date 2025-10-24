a48c8d80f8d729ed01abb31a3411650950abc069
Denis Badurina <denis@denelop.com>
2025-10-24 16:07:12 +0200

    consumer schema

--- packages/fusion-runtime/src/router/handler.ts
+++ packages/fusion-runtime/src/router/handler.ts
@@ -4,7 +4,7 @@ import {
   type UnifiedGraphHandlerResult,
 } from '@graphql-mesh/fusion-runtime';
 import { memoize1, printSchemaWithDirectives } from '@graphql-tools/utils';
-import { DocumentNode, print } from 'graphql';
+import { buildSchema, DocumentNode, print } from 'graphql';
 import { handleFederationSupergraph } from '../federation/supergraph';
 import { executeQueryPlan } from './executor';
 
@@ -14,27 +14,27 @@ export function handleFederationSupergraphWithRouter(
   // TODO: should we do it this way? we only need the tools handler to pluck out the subgraphs
   const { getSubgraphSchema } = handleFederationSupergraph(opts);
 
-  const unifiedGraph = opts.unifiedGraph;
-
-  const qp = new QueryPlanner(printSchemaWithDirectives(unifiedGraph));
+  const supergraphSchema = opts.unifiedGraph;
+  const qp = new QueryPlanner(printSchemaWithDirectives(supergraphSchema));
+  const consumerSchemaSdl = qp.consumerSchema;
   const plan = memoize1(function plan(document: DocumentNode) {
     return qp.plan(print(document));
   });
   return {
-    unifiedGraph,
+    unifiedGraph: buildSchema(consumerSchemaSdl),
     getSubgraphSchema,
     executor({ document, variables, operationName, context }) {
-      return plan(document).then((queryPlan) =>
-        executeQueryPlan({
-          supergraphSchema: unifiedGraph,
+      return plan(document).then((queryPlan) => {
+        return executeQueryPlan({
+          supergraphSchema,
           document,
           operationName,
           variables,
           context,
           onSubgraphExecute: opts.onSubgraphExecute,
           queryPlan,
-        }),
-      );
+        });
+      });
     },
     inContextSDK: {
       // TODO: do we need/want an SDK here?
