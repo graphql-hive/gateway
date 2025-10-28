da9fdf742066bb619e0521acb493509c639b2ed3
Denis Badurina <denis@denelop.com>
2025-10-28 13:34:54 +0100

    filter internal types and fields

--- packages/fusion-runtime/src/router/handler.ts
+++ packages/fusion-runtime/src/router/handler.ts
@@ -3,21 +3,13 @@ import {
   type UnifiedGraphHandlerOpts,
   type UnifiedGraphHandlerResult,
 } from '@graphql-mesh/fusion-runtime';
+import { filterInternalFieldsAndTypes } from '@graphql-tools/federation';
 import {
   ExecutionResult,
   memoize1,
   printSchemaWithDirectives,
 } from '@graphql-tools/utils';
-import {
-  BREAK,
-  buildSchema,
-  DocumentNode,
-  execute,
-  GraphQLSchema,
-  print,
-  printSchema,
-  visit,
-} from 'graphql';
+import { BREAK, DocumentNode, execute, print, visit } from 'graphql';
 import { handleFederationSupergraph } from '../federation/supergraph';
 import { executeQueryPlan } from './executor';
 
@@ -26,20 +18,19 @@ export function handleFederationSupergraphWithRouter(
 ): UnifiedGraphHandlerResult {
   // TODO: should we do it this way? we only need the tools handler to pluck out the subgraphs
   const { getSubgraphSchema } = handleFederationSupergraph(opts);
-  const supergraphSchema = opts.unifiedGraph;
-  const qp = new QueryPlanner(printSchemaWithDirectives(supergraphSchema));
+  const qp = new QueryPlanner(printSchemaWithDirectives(opts.unifiedGraph));
   const plan = memoize1(function plan(document: DocumentNode) {
     return qp.plan(print(document));
   });
-  const consumerSchema = buildAndRemoveIntrospectionFields(qp.consumerSchema);
+  const supergraphSchema = filterInternalFieldsAndTypes(opts.unifiedGraph);
   return {
-    unifiedGraph: consumerSchema,
+    unifiedGraph: supergraphSchema,
     getSubgraphSchema,
     executor({ document, variables, operationName, context }) {
       if (isIntrospection(document)) {
         // TODO: handle introspection fields with data fields where also the query planner needs to run
         return execute({
-          schema: consumerSchema,
+          schema: supergraphSchema,
           document,
           variableValues: variables,
           operationName,
@@ -90,17 +81,4 @@ function isIntrospection(document: DocumentNode): boolean {
     },
   });
   return containsIntrospectionField || onlyQueryTypenameFields;
-}
-
-function buildAndRemoveIntrospectionFields(schemaSdl: string): GraphQLSchema {
-  // we print a built schema because print will remove introspection types
-  let saneSchemaSdl = printSchema(
-    buildSchema(schemaSdl, { assumeValid: true }),
-  );
-
-  // introspection fields in the query type will stay, remove them manually
-  saneSchemaSdl = saneSchemaSdl.replace('__schema: __Schema!', '');
-  saneSchemaSdl = saneSchemaSdl.replace('__type(name: String!): __Type', '');
-
-  return buildSchema(saneSchemaSdl, { assumeValid: true });
 }
