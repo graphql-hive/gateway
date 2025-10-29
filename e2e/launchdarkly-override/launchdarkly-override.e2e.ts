5be4ae010526e29c140019f75d59a9c71ac29b68
Valentin Cocaud <v.cocaud@gmail.com>
2025-10-29 21:10:12 +0100

    fix e2e launchdarkly test (#1647)
    Co-authored-by: theguild-bot <bot@the-guild.dev>

--- e2e/launchdarkly-override/launchdarkly-override.e2e.ts
+++ e2e/launchdarkly-override/launchdarkly-override.e2e.ts
@@ -8,6 +8,16 @@ const { gateway, service } = createTenv(__dirname);
 describe('LaunchDarkly Override E2E Test', () => {
   it('runs', async () => {
     await gateway({
+      runner: {
+        docker: {
+          volumes: [
+            {
+              host: `${__dirname}/node_modules`,
+              container: `/gateway/node_modules`,
+            },
+          ],
+        },
+      },
       supergraph: {
         with: 'apollo',
         services: [
