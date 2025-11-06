c754a96d49ea69f54f57a8f1b01baf9d2fb947b6
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-11-06 21:58:59 +0300

    enhance: better error codes

--- packages/runtime/src/plugins/useUpstreamTimeout.ts
+++ packages/runtime/src/plugins/useUpstreamTimeout.ts
@@ -105,7 +105,7 @@ export function useUpstreamTimeout<TContext extends Record<string, any>>(
                 throw createGraphQLError(e.message, {
                   extensions: {
                     ...upstreamErrorExtensions,
-                    code: 'TIMEOUT_ERROR',
+                    code: 'GATEWAY_TIMEOUT',
                     http: {
                       status: 504,
                     },
