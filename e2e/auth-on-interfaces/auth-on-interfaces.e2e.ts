95eeca776232502bd448e82c2913c75dec280a72
enisdenjo <badurinadenis@gmail.com>
2025-11-10 11:52:50 +0100

    allow vs deny

--- e2e/auth-on-interfaces/auth-on-interfaces.e2e.ts
+++ e2e/auth-on-interfaces/auth-on-interfaces.e2e.ts
@@ -1,9 +1,21 @@
 import { createTenv } from '@internal/e2e';
+import jwt from 'jsonwebtoken';
 import { expect, it } from 'vitest';
+import { JWT_SECRET } from './env';
 
 const { gateway, service } = createTenv(__dirname);
 
-it('should require all interface and implementor scopes', async () => {
+type Scope = 'a' | 'b' | 'i';
+
+it.each<{
+  query: string;
+  allow: Scope[];
+}>([
+  {
+    query: '{ i { id } }',
+    allow: ['i', 'a', 'b'],
+  },
+])('should allow $allow to query $query', async ({ query, allow }) => {
   const gw = await gateway({
     supergraph: {
       with: 'apollo',
@@ -13,21 +25,46 @@ it('should require all interface and implementor scopes', async () => {
 
   await expect(
     gw.execute({
-      query: /* GraphQL */ `
-        {
-          i {
-            id
-          }
-        }
-      `,
+      query,
+      headers: {
+        Authorization: `Bearer ${jwt.sign({ scope: allow }, JWT_SECRET)}`,
+      },
     }),
-  ).resolves.toMatchInlineSnapshot(`
-    {
-      "errors": [
-        {
-          "message": "Unauthenticated",
-        },
+  ).resolves.toEqual({
+    data: expect.any(Object),
+  });
+});
+
+it.each<{
+  query: string;
+  deny: Scope[];
+}>([
+  {
+    query: '{ i { id } }',
+    deny: ['i'],
+  },
+])('should deny $deny to query $query', async ({ query, deny }) => {
+  const gw = await gateway({
+    supergraph: {
+      with: 'apollo',
+      services: [await service('protected-req-on-int')],
+    },
+  });
+
+  await expect(
+    gw.execute({
+      query,
+      headers: {
+        Authorization: `Bearer ${jwt.sign({ scope: deny }, JWT_SECRET)}`,
+      },
+    }),
+  ).resolves.toEqual(
+    expect.objectContaining({
+      errors: [
+        expect.objectContaining({
+          message: 'Unauthorized field or type',
+        }),
       ],
-    }
-  `);
+    }),
+  );
 });
