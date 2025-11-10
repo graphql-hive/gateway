7b81b61803a61c9a5149e22dc01598f56acc0de4
enisdenjo <badurinadenis@gmail.com>
2025-11-10 12:02:40 +0100

    ok good

--- e2e/auth-on-interfaces/auth-on-interfaces.e2e.ts
+++ e2e/auth-on-interfaces/auth-on-interfaces.e2e.ts
@@ -1,70 +1,65 @@
 import { createTenv } from '@internal/e2e';
 import jwt from 'jsonwebtoken';
-import { expect, it } from 'vitest';
+import { it } from 'vitest';
 import { JWT_SECRET } from './env';
 
 const { gateway, service } = createTenv(__dirname);
 
+it.concurrent.for([
+  '{ i { id } }',
+  '{ i { ... on A { id } }',
+  '{ i { ... on A { id } ... on B { id } }',
+  '{ a { id } }',
+  '{ a { a } }',
+])(
+  'should decide authorization scopes for query %s',
+  async (query, { expect }) => {
+    const gw = await gateway({
+      supergraph: {
+        with: 'apollo',
+        services: [await service('protected-req-on-int')],
+      },
+    });
+
+    const scopes: {
+      allowed: Scope[][];
+      denied: Scope[][];
+    } = {
+      allowed: [],
+      denied: [],
+    };
+    for (const scope of getPossibleScopes()) {
+      const res = await gw.execute({
+        query,
+        headers: {
+          Authorization: `Bearer ${jwt.sign({ scope }, JWT_SECRET)}`,
+        },
+      });
+      if (res.data && !res.errors) {
+        scopes.allowed.push(scope);
+      } else {
+        scopes.denied.push(scope);
+      }
+    }
+
+    expect(scopes).toMatchSnapshot();
+  },
+);
+
 type Scope = 'a' | 'b' | 'i';
 
-it.each<{
-  query: string;
-  allow: Scope[];
-}>([
-  {
-    query: '{ i { id } }',
-    allow: ['i', 'a', 'b'],
-  },
-])('should allow $allow to query $query', async ({ query, allow }) => {
-  const gw = await gateway({
-    supergraph: {
-      with: 'apollo',
-      services: [await service('protected-req-on-int')],
-    },
-  });
-
-  await expect(
-    gw.execute({
-      query,
-      headers: {
-        Authorization: `Bearer ${jwt.sign({ scope: allow }, JWT_SECRET)}`,
-      },
-    }),
-  ).resolves.toEqual({
-    data: expect.any(Object),
-  });
-});
-
-it.each<{
-  query: string;
-  deny: Scope[];
-}>([
-  {
-    query: '{ i { id } }',
-    deny: ['i'],
-  },
-])('should deny $deny to query $query', async ({ query, deny }) => {
-  const gw = await gateway({
-    supergraph: {
-      with: 'apollo',
-      services: [await service('protected-req-on-int')],
-    },
-  });
-
-  await expect(
-    gw.execute({
-      query,
-      headers: {
-        Authorization: `Bearer ${jwt.sign({ scope: deny }, JWT_SECRET)}`,
-      },
-    }),
-  ).resolves.toEqual(
-    expect.objectContaining({
-      errors: [
-        expect.objectContaining({
-          message: 'Unauthorized field or type',
-        }),
-      ],
-    }),
-  );
-});
+function getPossibleScopes(): Scope[][] {
+  const scopes: Scope[] = ['a', 'b', 'i'];
+  const result: Scope[][] = [];
+  const n = scopes.length;
+  for (let i = 0; i < 1 << n; i++) {
+    const subset: Scope[] = [];
+    for (let j = 0; j < n; j++) {
+      if (i & (1 << j)) {
+        subset.push(scopes[j]!);
+      }
+    }
+    result.push(subset);
+  }
+  return result;
+}
