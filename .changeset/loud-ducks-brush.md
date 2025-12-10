db20a298c57c717495cbdbcaa6a8aa5b369da909
Denis Badurina <badurinadenis@gmail.com>
2025-12-10 16:46:07 +0100

    Do not print token info by default when using Hive Console (#1781)

---
+++ .changeset/loud-ducks-brush.md
@@ -0,0 +1,5 @@
+---
+'@graphql-hive/gateway-runtime': patch
+---
+
+Do not print token info by default when using Hive Console
--- packages/runtime/src/plugins/useHiveConsole.ts
+++ packages/runtime/src/plugins/useHiveConsole.ts
@@ -2,7 +2,6 @@ import type { HivePluginOptions } from '@graphql-hive/core';
 import type { Logger } from '@graphql-hive/logger';
 import { useHive } from '@graphql-hive/yoga';
 import { MeshFetch } from '@graphql-mesh/types';
-import { isDebug } from '~internal/env';
 import { GatewayPlugin } from '../types';
 
 export interface HiveConsolePluginOptions extends Omit<
@@ -75,8 +74,8 @@ export default function useHiveConsole<
 
   // @ts-expect-error TODO: useHive plugin should inhert the TContext
   return useHive({
-    debug: isDebug(),
     logger: options.log,
+    printTokenInfo: false,
     ...options,
     enabled: !!enabled,
     token: token!,
