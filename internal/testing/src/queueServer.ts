4e5e600a19ac55e350f24460a043933dd5bfb31f
Denis Badurina <denis@denelop.com>
2026-02-25 16:14:47 +0100

    hmm looking nice right?

--- internal/testing/src/queueServer.ts
+++ internal/testing/src/queueServer.ts
@@ -1,5 +1,9 @@
 import { createDisposableServer } from '@internal/testing';
-import { createServerAdapter, DisposableSymbols } from '@whatwg-node/server';
+import {
+  createDeferredPromise,
+  DeferredPromise,
+} from '@whatwg-node/promise-helpers';
+import { createServerAdapter } from '@whatwg-node/server';
 
 export type QueuedHandler = (req: Request) => Response | Promise<Response>;
 
@@ -8,30 +12,41 @@ export interface QueueServer {
   queue(handler: QueuedHandler): Promise<Response>;
 }
 
+interface QueueEntry {
+  handler: QueuedHandler;
+  responseDeferred: DeferredPromise<Response>;
+}
+
 export async function createQueueServer(): Promise<QueueServer> {
-  const queuedHandlers: QueuedHandler[] = [];
+  const queuedEntries: QueueEntry[] = [];
+  let entryAvailable = createDeferredPromise<void>();
 
   const serv = await createDisposableServer(
     createServerAdapter(async (req) => {
-      const handler = queuedHandlers.pop();
-      if (!handler) {
-        throw new Error('TODO: wait for handler to be queueud');
+      if (!queuedEntries.length) {
+        await entryAvailable.promise; // wait for available entry
+        entryAvailable = createDeferredPromise<void>(); // prepare next waiter
       }
-      return handler(req);
+
+      const entry = queuedEntries.shift()!;
+      try {
+        const response = await entry.handler(req);
+        entry.responseDeferred.resolve(response);
+        return response;
+      } catch (err) {
+        entry.responseDeferred.reject(err);
+        throw err;
+      }
     }),
   );
-
-  // stop the iterator when the server gets disposed
-  const origDispose = serv[DisposableSymbols.asyncDispose];
-  serv[DisposableSymbols.asyncDispose] = async () => {
-    stop();
-    await origDispose.call(serv);
-  };
 
   return {
     url: serv.url,
     queue(handler: QueuedHandler) {
-      // TODO: the handler to the queue, wait for it to be called, and return the response
+      const responseDeferred = createDeferredPromise<Response>();
+      queuedEntries.push({ handler, responseDeferred });
+      entryAvailable.resolve();
+      return responseDeferred.promise;
     },
   };
 }
