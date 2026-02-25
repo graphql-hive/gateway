1adf21b936587294fcd69b2c21da1bcc8e295728
Denis Badurina <denis@denelop.com>
2026-02-25 15:57:46 +0100

    iterable server and stuff

--- e2e/opentelemetry-circuit-breaker/gateway.config.ts
+++ e2e/opentelemetry-circuit-breaker/gateway.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from '@graphql-hive/gateway';
+import { defineConfig, GatewayPlugin } from '@graphql-hive/gateway';
 import { hiveTracingSetup } from '@graphql-hive/plugin-opentelemetry/setup';
 import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
+import FakeTimers from '@sinonjs/fake-timers';
 
 hiveTracingSetup({
   contextManager: new AsyncLocalStorageContextManager(),
@@ -13,4 +14,24 @@ export const gatewayConfig = defineConfig({
   openTelemetry: {
     traces: true,
   },
+  plugins: () => [useFakeTimers()],
 });
+
+// testing
+
+// this process will run in a separate process from vite so we cant use vite's fake timers
+const clock = FakeTimers.install({
+  // time should pass as usual, avoid disrupting the gateway
+  shouldAdvanceTime: true,
+});
+function useFakeTimers(): GatewayPlugin {
+  return {
+    async onRequest({ request, endResponse }) {
+      if (request.url.endsWith('/_tick')) {
+        const time = await request.json();
+        clock.tick(time);
+        endResponse(new Response());
+      }
+    },
+  };
+}
--- e2e/opentelemetry-circuit-breaker/opentelemetry-circuit-breaker.e2e.ts
+++ e2e/opentelemetry-circuit-breaker/opentelemetry-circuit-breaker.e2e.ts
@@ -1,39 +1,75 @@
-import { setTimeout } from 'node:timers/promises';
-import { createExampleSetup, createTenv } from '@internal/e2e';
+import { createExampleSetup, createTenv, Gateway } from '@internal/e2e';
 import { createDisposableServer } from '@internal/testing';
-import { createServerAdapter } from '@whatwg-node/server';
+import { Push, Repeater, Stop } from '@repeaterjs/repeater';
+import { fetch } from '@whatwg-node/fetch';
+import { createServerAdapter, DisposableSymbols } from '@whatwg-node/server';
 import { expect, it } from 'vitest';
 
 const { gateway } = createTenv(__dirname);
 const { supergraph } = createExampleSetup(__dirname);
 
 it('should huh?', async () => {
-  let count = 0;
-  const otel = await createDisposableServer(
-    createServerAdapter(async (req) => {
-      console.log({
-        count: ++count,
-        method: req.method,
-        headers: Object.fromEntries(req.headers.entries()),
-        // body: await req.text(),
-      });
-      if (count >= 5) {
-        return new Response('Service Unavailable', { status: 503 });
-      }
-      return new Response();
-    }),
-  );
+  const otel = await createIterableServer();
 
-  const { execute } = await gateway({
+  const gw = await gateway({
     supergraph: await supergraph(),
     env: {
       HIVE_TRACING_ENDPOINT: otel.url,
     },
   });
 
-  await expect(execute({ query: '{ __typename }' })).resolves.toEqual(
+  await expect(gw.execute({ query: '{ __typename }' })).resolves.toEqual(
     expect.objectContaining({ data: expect.any(Object) }),
   );
 
-  await setTimeout(30_000);
+  // batch exporter scheduledDelayMillis defaults to 5s
+  await advanceGatewayTimersByTime(gw, 5_000);
+
+  await otel.waitForRequest();
 });
+
+async function advanceGatewayTimersByTime(gateway: Gateway, timeInMs: number) {
+  const res = await fetch(`http://localhost:${gateway.port}/_tick`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(timeInMs),
+  });
+  if (!res.ok) {
+    throw new Error(`Failed to advance gateway timers: ${res.statusText}`);
+  }
+}
+
+async function createIterableServer() {
+  let push: Push<Request, Response>;
+  let stop: Stop;
+  const rep = new Repeater<Request, unknown, Response>((_push, _stop) => {
+    push = _push;
+    stop = _stop;
+  });
+
+  const serv = await createDisposableServer(
+    createServerAdapter(async (req) => {
+      const res = await push(req);
+      if (res) return res;
+      return new Response();
+    }),
+  );
+
+  // stop the iterator when the server gets disposed
+  const origDispose = serv[DisposableSymbols.asyncDispose];
+  serv[DisposableSymbols.asyncDispose] = async () => {
+    stop();
+    await origDispose.call(serv);
+  };
+
+  return Object.assign(rep, {
+    url: serv.url,
+    waitForRequest: (res?: Response) =>
+      rep.next(res).then(({ done, value }) => {
+        if (done) throw new Error('Server iterator stopped');
+        return value;
+      }),
+  });
+}
