c713e74042efc67a289bdf18afaa23962215d1bc
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-03-04 16:13:50 +0300

    Fix comment

--- packages/gateway/src/bin.ts
+++ packages/gateway/src/bin.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-// inject dotenv options to process.env
-
 import module from 'node:module';
 import type { InitializeData } from '@graphql-hive/importer/hooks';
 import { Logger } from '@graphql-hive/logger';
 import { enableModuleCachingIfPossible, handleNodeWarnings, run } from './cli';
 
+// inject dotenv options to process.env
 process.loadEnvFile();
 
 globalThis.__VERSION__ = 'dev';
