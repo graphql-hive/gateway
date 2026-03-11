ff22f7a6fc3a897b634af97a01fc16b1133a526a
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-03-11 17:33:15 +0300

    Fix Docker E2Es

--- e2e/openapi-javascript-wiki/gateway.config.ts
+++ e2e/openapi-javascript-wiki/gateway.config.ts
@@ -1,5 +1,44 @@
 import { defineConfig } from '@graphql-hive/gateway';
-import { additionalResolvers } from './additionalResolvers';
+import { BigIntResolver } from 'graphql-scalars';
+import { Resolvers } from './types/resolvers';
+
+const additionalResolvers: Resolvers = {
+  BigInt: BigIntResolver,
+  Query: {
+    async viewsInPastMonth(root, { start, end, project }, context, info) {
+      const result =
+        await context.Wiki.Query.metrics_pageviews_aggregate_by_project_by_access_by_agent_by_granularity_by_start_by_end(
+          {
+            root,
+            args: {
+              access: 'all_access',
+              agent: 'user',
+              start,
+              end,
+              project,
+              granularity: 'daily',
+            },
+            context,
+            info,
+            selectionSet: /* GraphQL */ `
+              {
+                ... on pageview_project {
+                  items {
+                    views
+                  }
+                }
+              }
+            `,
+          },
+        );
+
+      return (result?.items || []).reduce(
+        (sum, item) => sum + (item?.views ? BigInt(item.views) : 0n),
+        0n,
+      );
+    },
+  },
+};
 
 export const gatewayConfig = defineConfig({
   additionalResolvers,
