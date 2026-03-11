11af484a0a643a5a96f91199379d366483f3b3df
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-03-11 11:09:32 +0300

    Addressed

--- packages/batch-execute/src/splitResult.ts
+++ packages/batch-execute/src/splitResult.ts
@@ -14,7 +14,11 @@ export function splitResult(
 
   if (data) {
     for (const prefixedKey in data) {
-      if (prefixedKey === '__proto__' || prefixedKey === 'constructor' || prefixedKey === 'prototype') {
+      if (
+        prefixedKey === '__proto__' ||
+        prefixedKey === 'constructor' ||
+        prefixedKey === 'prototype'
+      ) {
         continue;
       }
       const { index, originalKey } = parseKey(prefixedKey);
