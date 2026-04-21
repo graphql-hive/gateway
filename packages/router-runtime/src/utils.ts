9fe008aa89b6ccb375dcf8b4b39a4d8b8341f13d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-04-21 14:21:18 +0300

    Cleanup

--- packages/router-runtime/src/utils.ts
+++ packages/router-runtime/src/utils.ts
@@ -33,43 +33,6 @@ export const queryPlanForExecutionRequestContext = new WeakMap<
   QueryPlan
 >();
 
-export function getLazyPromise<T>(
-  factory: () => MaybePromise<T>,
-): () => MaybePromise<T> {
-  let _value: MaybePromise<T>;
-  return function () {
-    if (_value == null) {
-      _value = handleMaybePromise(factory, (value) => {
-        _value = value;
-        return value;
-      });
-    }
-    return _value;
-  };
-}
-
-export function getLazyValue<T>(factory: () => T): () => T {
-  let _value: T;
-  return function () {
-    if (_value == null) {
-      _value = factory();
-    }
-    return _value;
-  };
-}
-
-export function getLazyFactory<T extends (...args: any) => any>(
-  factory: () => T,
-): T {
-  let _value: T;
-  return function (...args: Parameters<T>): ReturnType<T> {
-    if (!_value) {
-      _value = factory();
-    }
-    return _value(...args);
-  } as T;
-}
-
 const getEntityResolutionNodes = memoize1(function isEntityResolutionRequest(
   document: DocumentNode,
 ): FieldNode[] {
