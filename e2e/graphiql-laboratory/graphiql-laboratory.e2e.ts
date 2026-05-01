dcab26fb04d1653999dc9f0dea91af663f9746ee
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-05-01 16:35:39 +0300

    ..

--- e2e/graphiql-laboratory/graphiql-laboratory.e2e.ts
+++ e2e/graphiql-laboratory/graphiql-laboratory.e2e.ts
@@ -2,162 +2,167 @@ import { createTenv } from '@internal/e2e';
 import { getLocalhost } from '@internal/testing';
 import { describe, it } from 'vitest';
 
-describe('GraphiQL / Laboratory options', async () => {
-  const { gateway, service, composeWithMesh } = createTenv(__dirname);
-  const api = await service('api');
-  const { output: supergraph } = await composeWithMesh({
-    services: [api],
-    output: 'graphql',
-  });
-  const proxyUrl = `http://localhost:${api.port}/graphql`;
+const { gateway, service, composeWithMesh, gatewayRunner } =
+  createTenv(__dirname);
 
-  const opts = {
-    supergraph,
-    proxy: proxyUrl,
-  } as const;
-  for (const mode of Object.keys(opts) as (keyof typeof opts)[]) {
-    const url = opts[mode];
-    describe(mode, () => {
-      it('renders Laboratory by default', async () => {
-        const { port, protocol } = await gateway({
-          [mode]: url,
-        });
-        const hostname = await getLocalhost(port, protocol);
-        const response = await fetch(`${hostname}:${port}/graphql`, {
-          headers: {
-            accept: 'text/html',
-          },
-        });
-        const text = await response.text();
-        if (!text.includes('Laboratory')) {
-          throw new Error(`Expected Laboratory to be rendered by default`);
-        }
-      });
-      it('renders GraphiQL when --render-legacy-graphiql arg is set', async () => {
-        const { port, protocol } = await gateway({
-          [mode]: url,
-          args: ['--render-legacy-graphiql'],
-        });
-        const hostname = await getLocalhost(port, protocol);
-        const response = await fetch(`${hostname}:${port}/graphql`, {
-          headers: {
-            accept: 'text/html',
-          },
-        });
-        const text = await response.text();
-        if (!text.includes('GraphiQL')) {
-          throw new Error(
-            `Expected GraphiQL to be rendered when --render-legacy-graphiql is set`,
-          );
-        }
-      });
-      it('renders GraphiQL when RENDER_LEGACY_GRAPHIQL env var is set', async () => {
-        const { port, protocol } = await gateway({
-          [mode]: url,
-          env: { RENDER_LEGACY_GRAPHIQL: '1' },
-        });
-        const hostname = await getLocalhost(port, protocol);
-        const response = await fetch(`${hostname}:${port}/graphql`, {
-          headers: {
-            accept: 'text/html',
-          },
-        });
-        const text = await response.text();
-        if (!text.includes('GraphiQL')) {
-          throw new Error(
-            `Expected GraphiQL to be rendered when RENDER_LEGACY_GRAPHIQL env var is set`,
-          );
-        }
-      });
-      it('renders GraphiQL when renderLegacyGraphiQL is set via config file', async () => {
-        const { port, protocol } = await gateway({
-          [mode]: url,
-          env: { RENDER_LEGACY_GRAPHIQL_CONFIG: '1' },
-        });
-        const hostname = await getLocalhost(port, protocol);
-        const response = await fetch(`${hostname}:${port}/graphql`, {
-          headers: {
-            accept: 'text/html',
-          },
-        });
-        const text = await response.text();
-        if (!text.includes('GraphiQL')) {
-          throw new Error(
-            `Expected GraphiQL to be rendered when renderLegacyGraphiQL is set via config file`,
-          );
-        }
-      });
-      it('mentions Laboratory in the landing page by default', async () => {
-        const { port, protocol } = await gateway({
-          [mode]: url,
-        });
-        const hostname = await getLocalhost(port, protocol);
-        const response = await fetch(`${hostname}:${port}/`, {
-          headers: {
-            accept: 'text/html',
-          },
-        });
-        const text = await response.text();
-        if (!text.includes('Laboratory')) {
-          throw new Error(
-            `Expected landing page to mention Laboratory by default`,
-          );
-        }
-      });
-      it('mentions GraphiQL in the landing page when --render-legacy-graphiql arg is set', async () => {
-        const { port, protocol } = await gateway({
-          [mode]: url,
-          args: ['--render-legacy-graphiql'],
-        });
-        const hostname = await getLocalhost(port, protocol);
-        const response = await fetch(`${hostname}:${port}/`, {
-          headers: {
-            accept: 'text/html',
-          },
-        });
-        const text = await response.text();
-        if (!text.includes('GraphiQL')) {
-          throw new Error(
-            `Expected landing page to mention GraphiQL when --render-legacy-graphiql is set`,
-          );
-        }
-      });
-      it('mentions GraphiQL in the landing page when RENDER_LEGACY_GRAPHIQL env var is set', async () => {
-        const { port, protocol } = await gateway({
-          [mode]: url,
-          env: { RENDER_LEGACY_GRAPHIQL: '1' },
-        });
-        const hostname = await getLocalhost(port, protocol);
-        const response = await fetch(`${hostname}:${port}/`, {
-          headers: {
-            accept: 'text/html',
-          },
-        });
-        const text = await response.text();
-        if (!text.includes('GraphiQL')) {
-          throw new Error(
-            `Expected landing page to mention GraphiQL when RENDER_LEGACY_GRAPHIQL env var is set`,
-          );
-        }
-      });
-      it('mentions GraphiQL in the landing page when renderLegacyGraphiQL is set via config file', async () => {
-        const { port, protocol } = await gateway({
-          [mode]: url,
-          env: { RENDER_LEGACY_GRAPHIQL_CONFIG: '1' },
-        });
-        const hostname = await getLocalhost(port, protocol);
-        const response = await fetch(`${hostname}:${port}/`, {
-          headers: {
-            accept: 'text/html',
-          },
-        });
-        const text = await response.text();
-        if (!text.includes('GraphiQL')) {
-          throw new Error(
-            `Expected landing page to mention GraphiQL when renderLegacyGraphiQL is set via config file`,
-          );
-        }
-      });
+describe.skipIf(gatewayRunner === 'bin')(
+  'GraphiQL / Laboratory options',
+  async () => {
+    const api = await service('api');
+    const { output: supergraph } = await composeWithMesh({
+      services: [api],
+      output: 'graphql',
     });
-  }
-});
+    const proxyUrl = `http://localhost:${api.port}/graphql`;
+
+    const opts = {
+      supergraph,
+      proxy: proxyUrl,
+    } as const;
+    for (const mode of Object.keys(opts) as (keyof typeof opts)[]) {
+      const url = opts[mode];
+      describe(mode, () => {
+        it('renders Laboratory by default', async () => {
+          const { port, protocol } = await gateway({
+            [mode]: url,
+          });
+          const hostname = await getLocalhost(port, protocol);
+          const response = await fetch(`${hostname}:${port}/graphql`, {
+            headers: {
+              accept: 'text/html',
+            },
+          });
+          const text = await response.text();
+          if (!text.includes('Laboratory')) {
+            throw new Error(`Expected Laboratory to be rendered by default`);
+          }
+        });
+        it('renders GraphiQL when --render-legacy-graphiql arg is set', async () => {
+          const { port, protocol } = await gateway({
+            [mode]: url,
+            args: ['--render-legacy-graphiql'],
+          });
+          const hostname = await getLocalhost(port, protocol);
+          const response = await fetch(`${hostname}:${port}/graphql`, {
+            headers: {
+              accept: 'text/html',
+            },
+          });
+          const text = await response.text();
+          if (!text.includes('GraphiQL')) {
+            throw new Error(
+              `Expected GraphiQL to be rendered when --render-legacy-graphiql is set`,
+            );
+          }
+        });
+        it('renders GraphiQL when RENDER_LEGACY_GRAPHIQL env var is set', async () => {
+          const { port, protocol } = await gateway({
+            [mode]: url,
+            env: { RENDER_LEGACY_GRAPHIQL: '1' },
+          });
+          const hostname = await getLocalhost(port, protocol);
+          const response = await fetch(`${hostname}:${port}/graphql`, {
+            headers: {
+              accept: 'text/html',
+            },
+          });
+          const text = await response.text();
+          if (!text.includes('GraphiQL')) {
+            throw new Error(
+              `Expected GraphiQL to be rendered when RENDER_LEGACY_GRAPHIQL env var is set`,
+            );
+          }
+        });
+        it('renders GraphiQL when renderLegacyGraphiQL is set via config file', async () => {
+          const { port, protocol } = await gateway({
+            [mode]: url,
+            env: { RENDER_LEGACY_GRAPHIQL_CONFIG: '1' },
+          });
+          const hostname = await getLocalhost(port, protocol);
+          const response = await fetch(`${hostname}:${port}/graphql`, {
+            headers: {
+              accept: 'text/html',
+            },
+          });
+          const text = await response.text();
+          if (!text.includes('GraphiQL')) {
+            throw new Error(
+              `Expected GraphiQL to be rendered when renderLegacyGraphiQL is set via config file`,
+            );
+          }
+        });
+        it('mentions Laboratory in the landing page by default', async () => {
+          const { port, protocol } = await gateway({
+            [mode]: url,
+          });
+          const hostname = await getLocalhost(port, protocol);
+          const response = await fetch(`${hostname}:${port}/`, {
+            headers: {
+              accept: 'text/html',
+            },
+          });
+          const text = await response.text();
+          if (!text.includes('Laboratory')) {
+            throw new Error(
+              `Expected landing page to mention Laboratory by default`,
+            );
+          }
+        });
+        it('mentions GraphiQL in the landing page when --render-legacy-graphiql arg is set', async () => {
+          const { port, protocol } = await gateway({
+            [mode]: url,
+            args: ['--render-legacy-graphiql'],
+          });
+          const hostname = await getLocalhost(port, protocol);
+          const response = await fetch(`${hostname}:${port}/`, {
+            headers: {
+              accept: 'text/html',
+            },
+          });
+          const text = await response.text();
+          if (!text.includes('GraphiQL')) {
+            throw new Error(
+              `Expected landing page to mention GraphiQL when --render-legacy-graphiql is set`,
+            );
+          }
+        });
+        it('mentions GraphiQL in the landing page when RENDER_LEGACY_GRAPHIQL env var is set', async () => {
+          const { port, protocol } = await gateway({
+            [mode]: url,
+            env: { RENDER_LEGACY_GRAPHIQL: '1' },
+          });
+          const hostname = await getLocalhost(port, protocol);
+          const response = await fetch(`${hostname}:${port}/`, {
+            headers: {
+              accept: 'text/html',
+            },
+          });
+          const text = await response.text();
+          if (!text.includes('GraphiQL')) {
+            throw new Error(
+              `Expected landing page to mention GraphiQL when RENDER_LEGACY_GRAPHIQL env var is set`,
+            );
+          }
+        });
+        it('mentions GraphiQL in the landing page when renderLegacyGraphiQL is set via config file', async () => {
+          const { port, protocol } = await gateway({
+            [mode]: url,
+            env: { RENDER_LEGACY_GRAPHIQL_CONFIG: '1' },
+          });
+          const hostname = await getLocalhost(port, protocol);
+          const response = await fetch(`${hostname}:${port}/`, {
+            headers: {
+              accept: 'text/html',
+            },
+          });
+          const text = await response.text();
+          if (!text.includes('GraphiQL')) {
+            throw new Error(
+              `Expected landing page to mention GraphiQL when renderLegacyGraphiQL is set via config file`,
+            );
+          }
+        });
+      });
+    }
+  },
+);
