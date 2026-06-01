501b0e1f42e8d9b862a7647942f2633d4da2d122
Denis Badurina <denis@denelop.com>
2026-06-01 16:01:14 +0200

    real tests

--- e2e/request-deadline/request-deadline.e2e.ts
+++ e2e/request-deadline/request-deadline.e2e.ts
@@ -3,7 +3,7 @@ import { expect, it } from 'vitest';
 
 const { gateway, service } = createTenv(__dirname);
 
-it('should do something', async () => {
+it('should respond when deadline reached', async () => {
   const gw = await gateway({
     supergraph: {
       with: 'apollo',
@@ -20,4 +20,37 @@ it('should do something', async () => {
   expect(err.message).toContain(
     'ResponseError: Status is not 200, got status 503 Service Unavailable',
   );
+});
+
+it('should invoke onResponse on deadline reached', async () => {
+  const gw = await gateway({
+    supergraph: {
+      with: 'apollo',
+      services: [await service('hello')],
+    },
+  });
+
+  const err: Error = await gw
+    .execute({
+      query: '{slowHello}',
+    })
+    .catch((err) => err);
+
+  expect(err.message).toContain(
+    'ResponseError: Status is not 200, got status 503 Service Unavailable',
+  );
+
+  // wait for the subgraph timeout to pass to ensure onResponse is invoked (event if deadline does not work)
+  await new Promise((resolve) => setTimeout(resolve, 300));
+
+  const onResponseRawData = gw
+    .getStd('both')
+    .split('\n')
+    .find((line) => line.startsWith('[onResponse]'))
+    ?.replace('[onResponse]', '');
+  expect(onResponseRawData).toBeTruthy();
+
+  const onResponseData = JSON.parse(onResponseRawData!);
+  expect(onResponseData.statusCode).toBe(503); // the status code when deadline is reached
+  expect(onResponseData.durationInMs).toBeLessThan(150); // 100ms is the request deadline + some leeway
 });
